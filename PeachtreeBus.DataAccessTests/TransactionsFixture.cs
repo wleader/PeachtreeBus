8975d18b63782d60e1241bfaf8801f2e2e654ada
wleader <william.leader@gmail.com>
2021-02-07 00:15:17 -0500

    Added Tests on Transactions and Savepoints
    These are really more like a unit test, but there are here in the Data Access tests cause it makes more sense to me organizationally to have all the test on the DapperDataAccess class in one project.

---
+++ PeachtreeBus.DataAccessTests/TransactionsFixture.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using PeachtreeBus.Data;
+using PeachtreeBus.DatabaseSharing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeachtreeBus.DataAccessTests
+{
+    [TestClass]
+    public class TransactionsFixture
+    {
+        private Mock<ISharedDatabase> MockSharedDatabase;
+        private DapperDataAccess dataAccess;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            MockSharedDatabase = new Mock<ISharedDatabase>();
+            dataAccess = new DapperDataAccess(MockSharedDatabase.Object, null);
+        }
+
+        [TestMethod]
+        public void StartTransaction_InvokesSharedDB()
+        {
+            MockSharedDatabase.Setup(db => db.BeginTransaction()).Verifiable();
+            dataAccess.BeginTransaction();
+            MockSharedDatabase.Verify();
+        }
+
+        [TestMethod]
+        public void CommitTransaction_InvokesSharedDB()
+        {
+            MockSharedDatabase.Setup(db => db.CommitTransaction()).Verifiable();
+            dataAccess.CommitTransaction();
+            MockSharedDatabase.Verify();
+        }
+
+        [TestMethod]
+        public void RollbackTransaction_InvokesSharedDB()
+        {
+            MockSharedDatabase.Setup(db => db.RollbackTransaction()).Verifiable();
+            dataAccess.RollbackTransaction();
+            MockSharedDatabase.Verify();
+        }
+
+        [TestMethod]
+        public void CreateSavepoint_InvokesSharedDB()
+        {
+            MockSharedDatabase.Setup(db => db.CreateSavepoint("Savepoint")).Verifiable();
+            dataAccess.CreateSavepoint("Savepoint");
+            MockSharedDatabase.Verify();
+        }
+
+        [TestMethod]
+        public void RollbackToSavepoint_InvokesSharedDB()
+        {
+            MockSharedDatabase.Setup(db => db.RollbackToSavepoint("Savepoint")).Verifiable();
+            dataAccess.RollbackToSavepoint("Savepoint");
+            MockSharedDatabase.Verify();
+        }
+    }
+}
