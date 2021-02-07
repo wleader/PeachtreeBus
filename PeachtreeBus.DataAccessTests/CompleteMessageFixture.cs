3ef5764d1be7e8d6ba64c365b6e286b0ffe7d8a0
wleader <william.leader@gmail.com>
2021-02-07 18:48:38 -0500

    Added Tests on Complete, Fail, and Update Message

---
+++ PeachtreeBus.DataAccessTests/CompleteMessageFixture.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeachtreeBus.DataAccessTests
+{
+    [TestClass]
+    public class CompleteMessageFixture : FixtureBase
+    {
+        [TestInitialize]
+        public override void TestInitialize()
+        {
+            base.TestInitialize();
+        }
+
+        [TestCleanup]
+        public override void TestCleanup()
+        {
+            base.TestCleanup();
+        }
+
+        [TestMethod]
+        public async Task CompleteMessage_InsertsIntoCompleteTable()
+        {
+            // Add two messages;
+            var testMessage1 = CreateTestMessage();
+            testMessage1.Id = await dataAccess.EnqueueMessage(testMessage1, DefaultQueue);
+            var testMessage2 = CreateTestMessage();
+            testMessage2.Id = await dataAccess.EnqueueMessage(testMessage2, DefaultQueue);
+            await Task.Delay(10); // wait for the rows to be ready
+
+            // get and complete a message.
+            var messageToComplete = await dataAccess.GetOnePendingMessage(DefaultQueue);
+            messageToComplete.Completed = DateTime.UtcNow;
+            await dataAccess.CompleteMessage(messageToComplete, DefaultQueue);
+            await Task.Delay(10); // wait for the rows to be ready
+
+            // Check that it ended up in the completed table.
+            var completed = GetTableContent(CompletedMessagesTable).ToMessages();
+            Assert.AreEqual(1, completed.Count);
+            AssertMessageEquals(messageToComplete, completed[0]);
+        }
+
+        [TestMethod]
+        public async Task CompleteMessage_DeletesFromPendingTable()
+        {
+            // Add two messages;
+            var testMessage1 = CreateTestMessage();
+            testMessage1.Id = await dataAccess.EnqueueMessage(testMessage1, DefaultQueue);
+            var testMessage2 = CreateTestMessage();
+            testMessage2.Id = await dataAccess.EnqueueMessage(testMessage2, DefaultQueue);
+            await Task.Delay(10); // wait for the rows to be ready
+
+            var messageToComplete = await dataAccess.GetOnePendingMessage(DefaultQueue);
+            messageToComplete.Completed = DateTime.UtcNow;
+            await dataAccess.CompleteMessage(messageToComplete, DefaultQueue);
+            await Task.Delay(10); // wait for the rows to be ready
+
+            var pending = GetTableContent(PendingMessagesTable).ToMessages();
+            Assert.AreEqual(1, pending.Count);
+            Assert.IsFalse(pending.Any(m => m.Id == messageToComplete.Id), "Completed message is still in the pending table.");
+        }
+
+        [TestMethod]
+        public async Task CompleteMessage_CantMutateFields()
+        {
+            // Add two messages;
+            var testMessage1 = CreateTestMessage();
+            testMessage1.Id = await dataAccess.EnqueueMessage(testMessage1, DefaultQueue);
+            await Task.Delay(10); // wait for the rows to be ready
+
+            // get and complete a message.
+            var messageToComplete = await dataAccess.GetOnePendingMessage(DefaultQueue);
+            messageToComplete.Completed = DateTime.UtcNow;
+            // screw with the fields that shouldn't change.
+            messageToComplete.Body = "NewBody";
+            messageToComplete.Enqueued = messageToComplete.Enqueued.AddMinutes(1);
+            messageToComplete.MessageId = Guid.NewGuid();
+
+            await dataAccess.CompleteMessage(messageToComplete, DefaultQueue);
+            await Task.Delay(10); // wait for the rows to be ready
+
+            // Check that it ended up in the completed table.
+            var completed = GetTableContent(CompletedMessagesTable).ToMessages();
+            Assert.AreEqual(1, completed.Count);
+            var actual = completed.Single(m => m.Id == testMessage1.Id);
+
+            // check the immutable fields are the oringal valules.
+            Assert.AreEqual(testMessage1.MessageId, actual.MessageId, "MessageId should not change.");
+            AssertSqlDbDateTime(testMessage1.Enqueued, actual.Enqueued);
+            Assert.AreEqual(testMessage1.Body, actual.Body, "Body should not change.");
+        }
+
+
+        [TestMethod]
+        public void CompleteMessage_ThrowsIfDateTimeKindUnspecified()
+        {
+            var action = new Action<Model.QueueMessage>((m) => dataAccess.CompleteMessage(m, DefaultQueue));
+            ActionThrowsForMessagesWithUnspecifiedDateTimeKinds(action);
+        }
+
+        [TestMethod]
+        public void CompleteMessage_ThrowsIfSchemaContainsUnsafe()
+        {
+            var action = new Action(() => dataAccess.CompleteMessage(new Model.QueueMessage(), DefaultQueue));
+            ActionThrowsIfSchemaContainsPoisonChars(action);
+        }
+
+        [TestMethod]
+        public void CompleteMessage_ThrowsIfQueueNameContainsUnsafe()
+        {
+            var action = new Action<string>((s) => dataAccess.CompleteMessage(new Model.QueueMessage(), s));
+            ActionThrowsIfParameterContainsPoisonChars(action);
+        }
+
+    }
+}
---
+++ PeachtreeBus.DataAccessTests/UpdateMessageFixture.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeachtreeBus.DataAccessTests
+{
+    [TestClass]
+    public class UpdateMessageFixture : FixtureBase
+    {
+        [TestInitialize]
+        public override void TestInitialize()
+        {
+            base.TestInitialize();
+        }
+
+        [TestCleanup]
+        public override void TestCleanup()
+        {
+            base.TestCleanup();
+        }
+
+        [TestMethod]
+        public async Task UpdateMessage_UpdatesPendingTable()
+        {
+            // Add two messages;
+            var testMessage1 = CreateTestMessage();
+            testMessage1.Id = await dataAccess.EnqueueMessage(testMessage1, DefaultQueue);
+            var testMessage2 = CreateTestMessage();
+            testMessage2.Id = await dataAccess.EnqueueMessage(testMessage2, DefaultQueue);
+            await Task.Delay(10); // wait for the rows to be ready
+
+            // get and update a message.
+            var toUpdate = await dataAccess.GetOnePendingMessage(DefaultQueue);
+            
+            // set changed values
+            toUpdate.MessageId = Guid.NewGuid(); // this should never persist a change.
+            toUpdate.Enqueued = toUpdate.Enqueued.AddMinutes(-1); // this should never change.
+            toUpdate.Body = "Changed Body"; // should never change.
+            toUpdate.Headers = "Changed Headers";
+            toUpdate.NotBefore = toUpdate.NotBefore.AddMinutes(1);
+            toUpdate.Completed = DateTime.UtcNow;
+            toUpdate.Failed = DateTime.UtcNow;
+            toUpdate.Retries = 10;
+
+            await dataAccess.Update(toUpdate, DefaultQueue);
+            await Task.Delay(10); // wait for the rows to be ready
+
+            // Check that it ended up in the error table.
+            var pending = GetTableContent(PendingMessagesTable).ToMessages();
+            Assert.AreEqual(2, pending.Count);
+
+            var expectUnchanged = toUpdate.Id == testMessage1.Id ? testMessage2 : testMessage1;
+            var changedOriginal = toUpdate.Id != testMessage1.Id ? testMessage2 : testMessage1;
+
+            var actualUnchanged = pending.Single(m => m.Id != toUpdate.Id);
+            AssertMessageEquals(expectUnchanged, actualUnchanged);
+
+            var actualChanged = pending.Single(m => m.Id == toUpdate.Id);
+            // compare the unchangable fields.
+            Assert.AreEqual(changedOriginal.Id, actualChanged.Id);
+            Assert.AreEqual(changedOriginal.MessageId, actualChanged.MessageId);
+            AssertSqlDbDateTime(changedOriginal.Enqueued, actualChanged.Enqueued);
+            Assert.AreEqual(changedOriginal.Body, actualChanged.Body);
+            // compare the changeable fields.
+            Assert.AreEqual(toUpdate.Headers, actualChanged.Headers);
+            AssertSqlDbDateTime(toUpdate.NotBefore, actualChanged.NotBefore);
+            AssertSqlDbDateTime(toUpdate.Completed, actualChanged.Completed);
+            AssertSqlDbDateTime(toUpdate.Failed, actualChanged.Failed);
+            Assert.AreEqual(toUpdate.Retries, actualChanged.Retries);
+
+        }
+
+
+        [TestMethod]
+        public void FailMessage_ThrowsIfDateTimeKindUnspecified()
+        {
+            var action = new Action<Model.QueueMessage>((m) => dataAccess.FailMessage(m, DefaultQueue));
+            ActionThrowsForMessagesWithUnspecifiedDateTimeKinds(action);
+        }
+
+        [TestMethod]
+        public void FailMessage_ThrowsIfSchemaContainsUnsafe()
+        {
+            var action = new Action(() => dataAccess.FailMessage(new Model.QueueMessage(), DefaultQueue));
+            ActionThrowsIfSchemaContainsPoisonChars(action);
+        }
+
+        [TestMethod]
+        public void FailMessage_ThrowsIfQueueNameContainsUnsafe()
+        {
+            var action = new Action<string>((s) => dataAccess.FailMessage(new Model.QueueMessage(), s));
+            ActionThrowsIfParameterContainsPoisonChars(action);
+        }
+
+    }
+}
