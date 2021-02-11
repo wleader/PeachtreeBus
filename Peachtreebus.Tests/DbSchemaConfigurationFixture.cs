9ad5114fc5cc7f5b80b4ead77275c43618111721
wleader <william.leader@gmail.com>
2021-02-10 20:49:44 -0500

    Created tests for DbSchemaConfiguration

---
+++ Peachtreebus.Tests/DbSchemaConfigurationFixture.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PeachtreeBus.Data;
+
+namespace Peachtreebus.Tests
+{
+    [TestClass]
+    public class DbSchemaConfigurationFixture
+    {
+        [TestMethod]
+        public void DbSchemaConfiguration_ReturnsSchemaNameProvidedToConstructor()
+        {
+            IDbSchemaConfiguration schema = new DbSchemaConfiguration("TestSchema");
+            Assert.AreEqual("TestSchema", schema.Schema);
+        }
+
+        [TestMethod]
+        public void DbSchemaConfiguration_UsesCorrectDefault()
+        {
+            IDbSchemaConfiguration schema = new DbSchemaConfiguration();
+            Assert.AreEqual("PeachtreeBus", schema.Schema);
+        }
+    }
+}
