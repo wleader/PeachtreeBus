f11638e5319875bbb8708d6119d66bcc31677116
wleader <william.leader@gmail.com>
2021-02-11 21:56:30 -0500

    Added Tests on MessageContext

---
+++ Peachtreebus.Tests/MessageContextFixture.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PeachtreeBus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Peachtreebus.Tests
+{
+    [TestClass]
+    public class MessageContextFixture
+    {
+        [TestMethod]
+        public void Send_AddsToSentMessages()
+        {
+            var context = new MessageContext();
+            var message = new object();
+            var notBefore = DateTime.UtcNow.AddMinutes(1);
+
+            Assert.AreEqual(0, context.SentMessages.Count);
+            context.Send(message, "QueueName", notBefore);
+
+            Assert.AreEqual(1, context.SentMessages.Count);
+
+            Assert.AreEqual(notBefore, context.SentMessages[0].NotBefore);
+            Assert.AreEqual("QueueName", context.SentMessages[0].QueueName);
+            Assert.IsTrue(ReferenceEquals(message, context.SentMessages[0].Message));
+            Assert.AreEqual(typeof(object), context.SentMessages[0].Type);
+        }
+    }
+}
