8d7231e796c9316fc5ea43111e0028aae4f83fdc
wleader <william.leader@gmail.com>
2022-03-05 11:52:12 -0500

    Create QueueCleanupWork.cs

---
+++ PeachtreeBus/Cleaners/QueueCleanupWork.cs
@@ -0,0 +1,21 @@
+﻿using PeachtreeBus.Interfaces;
+
+namespace PeachtreeBus.Cleaners
+{
+    /// <summary>
+    /// A unit of work for the IQueueCleanupThread.
+    /// </summary>
+    public interface IQueueCleanupWork : IUnitOfWork { }
+
+    /// <summary>
+    /// Default implmentation of IQueueCleanupWork
+    /// </summary>
+    public class QueueCleanupWork : BaseCleanupWork, IQueueCleanupWork
+    {
+        public QueueCleanupWork(IQueueCleanerConfiguration config,
+            ISystemClock clock,
+            IQueueCleaner cleaner)
+            : base(config, clock, cleaner)
+        { }
+    }
+}
