778c41f1f24474b32744c02a4d985b71f53facb0
wleader <william.leader@gmail.com>
2022-03-05 12:14:51 -0500

    Create IHandleSubscribedMessage.cs

---
+++ PeachtreeBus/Subscriptions/IHandleSubscribedMessage.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace PeachtreeBus.Subscriptions
+{
+    /// <summary>
+    /// Defines an interface for classes that will handle a message of a given type.
+    /// </summary>
+    /// <typeparam name="T">A message class</typeparam>
+    public interface IHandleSubscribedMessage<T> where T : ISubscribedMessage
+    {
+        Task Handle(SubscribedContext context, T message);
+    }
+}
