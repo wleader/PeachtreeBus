a6f2f80d27a693cdbf43759f6e566c3e52fe498d
wleader <william.leader@gmail.com>
2022-03-05 10:45:14 -0500

    Create SimpleInjectorScope.cs

---
+++ PeachtreeBus.SimpleInjector/SimpleInjectorScope.cs
@@ -0,0 +1,31 @@
+﻿using SimpleInjector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PeachtreeBus.SimpleInjector
+{
+    /// <summary>
+    /// An implementation of IWrappedScope for Simple Injector.
+    /// </summary>
+    public class SimpleInjectorScope : IWrappedScope
+    {
+        public Scope Scope { get; set; }
+
+        public IEnumerable<T> GetAllInstances<T>() where T : class
+        {
+            var instances = Scope.Container.GetAllInstances<T>();
+            return instances.Select(i => (T)Scope.GetInstance(i.GetType()));
+        }
+
+        public T GetInstance<T>() where T : class
+        {
+            return Scope.GetInstance<T>();
+        }
+
+        public object GetInstance(Type t)
+        {
+            return Scope.GetInstance(t);
+        }
+    }
+}
