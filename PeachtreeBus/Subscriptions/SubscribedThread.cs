657102e352f6c9c9931316ca6d7d8e67605ad1d9
wleader <william.leader@gmail.com>
2022-03-05 12:19:45 -0500

    Create SubscribedThread.cs

---
+++ PeachtreeBus/Subscriptions/SubscribedThread.cs
@@ -0,0 +1,34 @@
+﻿using PeachtreeBus.Data;
+using System.Threading.Tasks;
+
+namespace PeachtreeBus.Subscriptions
+{
+    /// <summary>
+    /// Describes a task that will read subscribed messages and attempt to process them.
+    /// </summary>
+    public interface ISubscribedThread : IThread { }
+   
+    /// <summary>
+    /// A task that will read subscribed messages and attempt to process them.
+    /// </summary>
+    public class SubscribedThread : BaseThread, ISubscribedThread
+    {
+        private readonly ISubscribedWork _subscribedWork;
+
+        public SubscribedThread(IProvideShutdownSignal provideShutdownSignal,
+            ILog<SubscribedThread> log,
+            IBusDataAccess transactionContext,
+            ISubscriberConfiguration subscriberConfiguration,
+            ISubscribedWork subscribeWork)
+            : base("Subscription Message", 100, log, transactionContext, provideShutdownSignal)
+        {
+            _subscribedWork = subscribeWork;
+            _subscribedWork.SubscriberId = subscriberConfiguration.SubscriberId;
+        }
+
+        public override Task<bool> DoUnitOfWork()
+        {
+            return _subscribedWork.DoWork();
+        }
+    }
+}
