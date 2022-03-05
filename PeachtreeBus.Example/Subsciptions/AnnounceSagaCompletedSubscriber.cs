f99e2b20e9f423fe1d874be59e7a255f31bcfe85
wleader <william.leader@gmail.com>
2022-03-05 10:07:28 -0500

    Create AnnounceSagaCompletedSubscriber.cs

---
+++ PeachtreeBus.Example/Subsciptions/AnnounceSagaCompletedSubscriber.cs
@@ -0,0 +1,27 @@
+﻿using PeachtreeBus.Example.Messages;
+using PeachtreeBus.Subscriptions;
+using System.Threading.Tasks;
+
+namespace PeachtreeBus.Example.Subsciptions
+{
+    /// <summary>
+    /// An example of handling a subscribed message.
+    /// </summary>
+    public class AnnounceSagaCompletedSubscriber : IHandleSubscribedMessage<AnnounceSagaCompleted>
+    {
+        private readonly ILog<AnnounceSagaCompletedSubscriber> _log;
+
+        public AnnounceSagaCompletedSubscriber(
+            ILog<AnnounceSagaCompletedSubscriber> log)
+        {
+            _log = log;
+        }
+
+        public Task Handle(SubscribedContext context, AnnounceSagaCompleted message)
+        {
+            // nothing fancy, just note that we handled the subscribed message.
+            _log.Info($"Subscriber {context.SubscriberId} got Saga complete announcement {message.AppId}");
+            return Task.CompletedTask;
+        }
+    }
+}
