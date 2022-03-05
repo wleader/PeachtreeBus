f04aa88e5dacc77092c17088dcfea9d6b3ec40dc
wleader <william.leader@gmail.com>
2022-03-05 11:54:56 -0500

    Create SubscribedCleanupWork.cs

---
+++ PeachtreeBus/Cleaners/SubscribedCleanupWork.cs
@@ -0,0 +1,22 @@
+﻿using PeachtreeBus.Interfaces;
+
+namespace PeachtreeBus.Cleaners
+{
+    /// <summary>
+    /// Describes A unit of work that cleans up subscribed messages
+    /// </summary>
+    public interface ISubscribedCleanupWork : IUnitOfWork { }
+
+    /// <summary>
+    /// A unit of work that cleans up subscribed messages
+    /// </summary>
+    public class SubscribedCleanupWork : BaseCleanupWork, ISubscribedCleanupWork
+    {
+
+        public SubscribedCleanupWork(IQueueCleanerConfiguration config,
+            ISystemClock clock,
+            ISubscribedCleaner cleaner)
+            : base(config, clock, cleaner)
+        { }
+    }
+}
