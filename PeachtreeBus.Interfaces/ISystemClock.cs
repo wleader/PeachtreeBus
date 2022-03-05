4bfcbd60a4bf6546617a29f85ec218f01a85cd1a
wleader <william.leader@gmail.com>
2022-03-05 10:15:04 -0500

    Create ISystemClock.cs

---
+++ PeachtreeBus.Interfaces/ISystemClock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PeachtreeBus.Interfaces
+{
+    /// <summary>
+    /// Defines an abstraction for the system clock.
+    /// Supports testable code.
+    /// </summary>
+    public interface ISystemClock
+    {
+        DateTime UtcNow { get; }
+    }
+
+    /// <summary>
+    /// A default implementation of ISystemClock
+    /// </summary>
+    public class SystemClock : ISystemClock
+    {
+        public DateTime UtcNow { get => DateTime.UtcNow; }
+    }
+}
