0dbd7a04a36d6811d641861a23abd121fcd74beb
wleader <william.leader@gmail.com>
2022-03-05 10:40:00 -0500

    Update RunPeachtreeBus.cs

--- PeachtreeBus.SimpleInjector/RunPeachtreeBus.cs
+++ PeachtreeBus.SimpleInjector/RunPeachtreeBus.cs
@@ -1,5 +1,9 @@
-﻿using SimpleInjector;
+﻿using PeachtreeBus.Cleaners;
+using PeachtreeBus.Queues;
+using PeachtreeBus.Subscriptions;
+using SimpleInjector;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace PeachtreeBus.SimpleInjector
@@ -7,21 +11,64 @@ namespace PeachtreeBus.SimpleInjector
     public static partial class SimpleInjectorExtensions
     {
         /// <summary>
-        /// Creates instances of IMesageProcessor and runs them.
+        /// Creates instances of registered threads.
         /// </summary>
         /// <param name="container"></param>
-        /// <param name="queueId">The queue the message processor will read from.</param>
-        /// <param name="concurrency">The number of copies of the processor to create.</param>
-        /// <returns>A task that ends when the message processors have all shut down.</returns>
-        public static IList<Task> PeachtreeBusMessageProcessorTasks (this Container container, string queueName, int concurrency)
+        /// <param name="concurrency">The number of copies of the IQueueThread to create.</param>
+        /// <returns>A List of Tasks to run.</returns>
+        public static IList<Task> PeachtreeBusThreadTasks(this Container container, int concurrency)
         {
+            var factory = container.GetInstance<IWrappedScopeFactory>();
             var tasks = new List<Task>();
+
+            tasks.AddIfRegistered<IQueueCleanupThread>(container, factory);
+            tasks.AddIfRegistered<ISubscribedCleanupThread>(container, factory);
+            tasks.AddIfRegistered<ISubscriptionCleanupThread>(container, factory);
+            tasks.AddIfRegistered<ISubscriptionUpdateThread>(container, factory);
+            tasks.AddIfRegistered<ISubscribedThread>(container, factory);
+
             for (var i = 0; i < concurrency; i++)
             {
-                var scope = container.GetInstance<IScopeManager>();
-                tasks.Add(scope.GetInstance<IMessageProcessor>().Run(queueName));
+                tasks.AddIfRegistered<IQueueThread>(container, factory);
             }
+
             return tasks;
+        }
+
+        private static void AddIfRegistered<T>(this List<Task> list, Container container, IWrappedScopeFactory scopeFactory)
+            where T : IThread
+        {
+            if (container.IsRegistered<T>())
+            {
+                var scope = scopeFactory.Create();
+                var p = (T)scope.GetInstance(typeof(T));
+                list.Add(p.Run());
+            }
+        }
+
+        /// <summary>
+        /// Gets the registered threads, starts them, then waits for all of them to complete.
+        /// </summary>
+        /// <param name="container"></param>
+        /// <param name="concurrency"></param>
+        public static void RunPeachtreeBus(this Container container, int? concurrency = null)
+        {
+            var c = concurrency ?? System.Environment.ProcessorCount * 2;
+
+            var tasks = container.PeachtreeBusThreadTasks(c).ToArray();
+
+            var threads = new List<System.Threading.Thread>();
+            foreach (var task in tasks)
+            {
+                var thread = new System.Threading.Thread(() => task.GetAwaiter().GetResult());
+                thread.Start();
+                threads.Add(thread);
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
         }
     }
 }
