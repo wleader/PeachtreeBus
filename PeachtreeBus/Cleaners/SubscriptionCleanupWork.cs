cd73eba1af3e67c1fe209c13360cad17107c62da
wleader <william.leader@gmail.com>
2022-03-05 11:56:18 -0500

    Create SubscriptionCleanupWork.cs

---
+++ PeachtreeBus/Cleaners/SubscriptionCleanupWork.cs
@@ -0,0 +1,29 @@
+﻿using PeachtreeBus.Data;
+using System.Threading.Tasks;
+
+namespace PeachtreeBus.Cleaners
+{
+    /// <summary>
+    /// Describes a unit of work that cleans up expired subscriptons.
+    /// </summary>
+    public interface ISubscriptionCleanupWork : IUnitOfWork { }
+    
+    /// <summary>
+    /// A unit of work that cleans up expired subscriptions.
+    /// </summary>
+    public class SubscriptionCleanupWork : ISubscriptionCleanupWork
+    {
+        private readonly IBusDataAccess _dataAccess;
+
+        public SubscriptionCleanupWork(IBusDataAccess dataAccess)
+        {
+            _dataAccess = dataAccess;
+        }
+
+        public async Task<bool> DoWork()
+        {
+            await _dataAccess.ExpireSubscriptions();
+            return true;
+        }
+    }
+}
