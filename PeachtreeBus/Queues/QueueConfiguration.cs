fee77a2a17febcd410a43a454872a0fd20d8e413
wleader <william.leader@gmail.com>
2022-03-05 12:10:05 -0500

    Create QueueConfiguration.cs

---
+++ PeachtreeBus/Queues/QueueConfiguration.cs
@@ -0,0 +1,24 @@
+﻿namespace PeachtreeBus.Queues
+{
+    /// <summary>
+    /// Provides configuration to threads about
+    /// which queue it should use.
+    /// </summary>
+    public interface IQueueConfiguration
+    {
+        string QueueName { get; }
+    }
+
+    /// <summary>
+    /// A default implementation of IQueueConfiguration
+    /// </summary>
+    public class QueueConfiguration : IQueueConfiguration
+    {
+        public string QueueName{get; private set;}
+
+        public QueueConfiguration(string queueName)
+        {
+            QueueName = queueName;
+        }
+    }
+}
