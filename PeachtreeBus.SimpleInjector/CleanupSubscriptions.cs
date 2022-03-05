cea6f847776f3e476daf62609019015df2f5f350
wleader <william.leader@gmail.com>
2022-03-05 10:22:52 -0500

    Create CleanupSubscriptions.cs

---
+++ PeachtreeBus.SimpleInjector/CleanupSubscriptions.cs
@@ -0,0 +1,20 @@
+﻿using PeachtreeBus.Cleaners;
+using SimpleInjector;
+
+namespace PeachtreeBus.SimpleInjector
+{
+    public static partial class SimpleInjectorExtensions
+    {
+        /// <summary>
+        /// Enables cleanup of the Subscriptions table. 
+        /// </summary>
+        /// <param name="container"></param>
+        /// <returns></returns>
+        public static Container CleanupSubscriptions(this Container container)
+        {
+            container.Register<ISubscriptionCleanupThread, SubscriptionCleanupThread>(Lifestyle.Scoped);
+            container.Register<ISubscriptionCleanupWork, SubscriptionCleanupWork>(Lifestyle.Scoped);
+            return container;
+        }
+    }
+}
