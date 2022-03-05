6cc05e94cd5240e7304933d25e6581dae539c60b
wleader <william.leader@gmail.com>
2022-03-05 10:51:03 -0500

    Create UsePeachtreeBusSubscriptions.cs

---
+++ PeachtreeBus.SimpleInjector/UsePeachtreeBusSubscriptions.cs
@@ -0,0 +1,32 @@
+﻿using PeachtreeBus.Subscriptions;
+using SimpleInjector;
+
+namespace PeachtreeBus.SimpleInjector
+{
+    public static partial class SimpleInjectorExtensions
+    {
+        /// <summary>
+        /// Enables the handling of subscribed messages.
+        /// </summary>
+        /// <param name="container"></param>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        public static Container UsePeachtreeBusSubscriptions(this Container container, ISubscriberConfiguration config)
+        {
+            // register our subscription message handlers
+            container.RegisterPeachtreeBusSubscriptionHandlers();
+
+            // register stuff needed to process subscribed messages.
+            container.Register(typeof(ISubscribedThread), typeof(SubscribedThread), Lifestyle.Scoped);
+            container.Register(typeof(ISubscribedWork), typeof(SubscribedWork), Lifestyle.Scoped);
+            container.Register(typeof(ISubscribedReader), typeof(SubscribedReader), Lifestyle.Scoped);
+            container.Register(typeof(IFindSubscribedHandlers), typeof(FindSubscriptionHandlers), Lifestyle.Scoped);
+            container.Register(typeof(ISubscriptionUpdateThread), typeof(SubscriptionUpdateThread), Lifestyle.Scoped);
+            container.Register(typeof(ISubscriptionUpdateWork), typeof(SubscriptionUpdateWork), Lifestyle.Scoped);
+
+            // register our subscription configuration.
+            container.Register(typeof(ISubscriberConfiguration), () => config, Lifestyle.Singleton);
+            return container;
+        }
+    }
+}
