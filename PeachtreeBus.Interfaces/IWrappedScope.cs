f12868e902993c59aaac2f8f230e427743010ae2
wleader <william.leader@gmail.com>
2022-03-05 10:14:22 -0500

    Create IWrappedScope.cs

---
+++ PeachtreeBus.Interfaces/IWrappedScope.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PeachtreeBus
+{
+    /// <summary>
+    /// Defines an interface for a Dependency Injection Scope.
+    /// Can create instances of objects from the DI Container
+    /// that are in the current scope as the IWrappedScope itself.
+    /// </summary>
+    public interface IWrappedScope
+
+    {
+        T GetInstance<T>() where T : class;
+        object GetInstance(Type t);
+        IEnumerable<T> GetAllInstances<T>() where T : class;
+    }
+
+    /// <summary>
+    /// Defines an interface for creating an IWrappedScope
+    /// </summary>
+    public interface IWrappedScopeFactory
+    {
+        IWrappedScope Create();
+    }
+}
