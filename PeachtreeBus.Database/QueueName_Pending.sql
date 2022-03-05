bb8cc005e736b3ee0777b142a1bb0da085905806
wleader <william.leader@gmail.com>
2022-03-05 09:43:38 -0500

    Create QueueName_Pending.sql

---
+++ PeachtreeBus.Database/QueueName_Pending.sql
@@ -0,0 +1,19 @@
+﻿-- reference definition of a queue messages table.
+-- a specifically named version of this table will need to exist for each queue in use.
+
+CREATE TABLE [PeachtreeBus].[QueueName_Pending]
+(
+	[Id] BIGINT NOT NULL IDENTITY PRIMARY KEY, 
+    [MessageId] UNIQUEIDENTIFIER NOT NULL, 
+    [NotBefore] DATETIME2 NOT NULL, 
+    [Enqueued] DATETIME2 NOT NULL,
+    [Completed] DATETIME2 NULL,
+    [Failed] DATETIME2 NULL, 
+    [Retries] TINYINT NOT NULL,
+    [Headers] NVARCHAR(MAX) NOT NULL,
+    [Body] NVARCHAR(MAX) NOT NULL
+)
+GO
+
+ALTER TABLE [PeachtreeBus].[QueueName_Pending] ADD DEFAULT ((0)) FOR [Retries]
+GO
