838cef946e9cdeed3c5e3cdcf4af5e682df38b44
wleader <william.leader@gmail.com>
2022-03-05 09:44:05 -0500

    Create Subscribed_Failed.sql

---
+++ PeachtreeBus.Database/Subscribed_Failed.sql
@@ -0,0 +1,21 @@
+﻿-- reference definition of a subscribed messages table.
+-- a specifically named version of this table will need to exist for each queue in use.
+
+CREATE TABLE [PeachtreeBus].[Subscribed_Failed]
+(
+    [Id] BIGINT NOT NULL PRIMARY KEY,
+    [SubscriberId] UNIQUEIDENTIFIER NOT NULL, 
+    [ValidUntil] DATETIME2 NOT NULL,
+    [MessageId] UNIQUEIDENTIFIER NOT NULL, 
+    [NotBefore] DATETIME2 NOT NULL, 
+    [Enqueued] DATETIME2 NOT NULL,
+    [Completed] DATETIME2 NULL,
+    [Failed] DATETIME2 NULL, 
+    [Retries] TINYINT NOT NULL,
+    [Headers] NVARCHAR(MAX) NOT NULL,
+    [Body] NVARCHAR(MAX) NOT NULL
+)
+GO
+
+ALTER TABLE [PeachtreeBus].[Subscribed_Failed] ADD DEFAULT ((0)) FOR [Retries]
+GO
