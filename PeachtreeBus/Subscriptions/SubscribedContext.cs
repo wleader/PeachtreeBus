6aa967f79f9ee1c0735569f937c16424ca2b126b
wleader <william.leader@gmail.com>
2022-03-05 12:15:49 -0500

    Create SubscribedContext.cs

---
+++ PeachtreeBus/Subscriptions/SubscribedContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PeachtreeBus.Subscriptions
+{
+    /// <summary>
+    /// stores contextual data about the subscription message being handled.
+    /// </summary>
+    public class SubscribedContext
+    {
+        /// <summary>
+        /// The message as read from the database.
+        /// </summary>
+        public Model.SubscribedMessage MessageData { get; set; }
+
+        /// <summary>
+        /// Headers that were stored with the message.
+        /// </summary>
+        public Headers Headers { get; set; }
+
+        /// <summary>
+        /// The message itself.
+        /// </summary>
+        public object Message { get; set; }
+
+        /// <summary>
+        /// The Subscriber that the message was sent to.
+        /// </summary>
+        public Guid SubscriberId { get; set; }
+    }
+}
