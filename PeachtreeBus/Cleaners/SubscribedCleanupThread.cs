4cc50a2ee3b007873dfa38124a5d441f7e3bcd65
wleader <william.leader@gmail.com>
2022-03-05 11:54:16 -0500

    Create SubscribedCleanupThread.cs

---
+++ PeachtreeBus/Cleaners/SubscribedCleanupThread.cs
@@ -0,0 +1,32 @@
+﻿using PeachtreeBus.Data;
+using System.Threading.Tasks;
+
+namespace PeachtreeBus.Cleaners
+{
+    /// <summary>
+    /// Describes a thread that cleans up subscribed messages.
+    /// </summary>
+    public interface ISubscribedCleanupThread : IThread { }
+
+    /// <summary>
+    /// A thread that cleans up subscribed messages.
+    /// </summary>
+    public class SubscribedCleanupThread : BaseThread, ISubscribedCleanupThread
+    {
+        private readonly ISubscribedCleanupWork _cleaner;
+
+        public SubscribedCleanupThread(ILog<SubscribedCleanupThread> log, 
+            IBusDataAccess dataAccess,
+            IProvideShutdownSignal shutdown,
+            ISubscribedCleanupWork cleaner) 
+            : base("SubscriptionCleaner", 500, log, dataAccess, shutdown)
+        {
+            _cleaner = cleaner;
+        }
+
+        public override Task<bool> DoUnitOfWork()
+        {
+           return _cleaner.DoWork();
+        }
+    }
+}
