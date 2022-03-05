3606f32c606e52aa2df9cf1fb57311d505cd144f
wleader <william.leader@gmail.com>
2022-03-05 09:11:32 -0500

    Create SagaDeleteFixture.cs

---
+++ PeachtreeBus.DataAccessTests/SagaDeleteFixture.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+
+namespace PeachtreeBus.DataAccessTests
+{
+    /// <summary>
+    /// Proves the behavior of DapperDataAccess.DeleteSagaData
+    /// </summary>
+    [TestClass]
+    public class SagaDeleteFixture :FixtureBase
+    {
+        [TestInitialize]
+        public override void TestInitialize()
+        {
+            base.TestInitialize();
+        }
+
+        [TestCleanup]
+        public override void TestCleanup()
+        {
+            base.TestCleanup();
+        }
+
+        /// <summary>
+        /// Proves that the correct row is deleted.
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task DeleteSaga_DeletesTheCorrectSaga()
+        {
+            var newSaga1 = CreateTestSagaData();
+            newSaga1.Key = "1";
+            var newSaga2 = CreateTestSagaData();
+            newSaga2.Key = "2";
+
+            newSaga1.Id = await dataAccess.Insert(newSaga1, DefaultSagaName);
+            newSaga2.Id = await dataAccess.Insert(newSaga2, DefaultSagaName);
+
+            await Task.Delay(10);
+            Assert.AreEqual(2, CountRowsInTable(DefaultSagaTable));
+
+            await dataAccess.DeleteSagaData(DefaultSagaName, newSaga1.Key);
+            await Task.Delay(10);
+
+            var sagas = GetTableContent(DefaultSagaTable).ToSagas();
+            Assert.AreEqual(1, sagas.Count);
+
+            AssertSagaEquals(newSaga2, sagas[0]);
+        }
+
+        /// <summary>
+        /// Proves that unsafe schema is not allowed.
+        /// </summary>
+        [TestMethod]
+        public void DeleteSaga_ThrowsIfSchemaContainsUnsafe()
+        {
+            var action = new Action(() => dataAccess.DeleteSagaData(DefaultSagaName, string.Empty));
+            ActionThrowsIfSchemaContainsPoisonChars(action);
+        }
+
+        /// <summary>
+        /// Proves that unsafe saga name is not allowed.
+        /// </summary>
+        [TestMethod]
+        public void DeleteSaga_ThrowsIfSagaNameContainsUnsafe()
+        {
+            var action = new Action<string>((s) => dataAccess.DeleteSagaData(s, string.Empty));
+            ActionThrowsIfParameterContainsPoisonChars(action);
+        }
+    }
+}
