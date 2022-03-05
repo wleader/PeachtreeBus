192ee8ea9110e1bb480ed3c84e48e76faae7e543
wleader <william.leader@gmail.com>
2022-03-05 10:40:47 -0500

    Create SimpleInjectorExtensions.cs

---
+++ PeachtreeBus.SimpleInjector/SimpleInjectorExtensions.cs
@@ -0,0 +1,18 @@
+﻿using SimpleInjector;
+
+namespace PeachtreeBus.SimpleInjector
+{
+    public static partial class SimpleInjectorExtensions
+    {
+        /// <summary>
+        /// Checks if a type is registered with the container.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="container"></param>
+        /// <returns></returns>
+        public static bool IsRegistered<T>(this Container container)
+        {
+            return container.GetRegistration(typeof(T)) != null;
+        }
+    }
+}
