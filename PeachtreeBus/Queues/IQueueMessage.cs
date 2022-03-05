4893e3ed5175a0a595ec8f17bb4b2dfde0a19cc4
wleader <william.leader@gmail.com>
2022-03-05 12:09:21 -0500

    Create IQueueMessage.cs

---
+++ PeachtreeBus/Queues/IQueueMessage.cs
@@ -0,0 +1,7 @@
+﻿namespace PeachtreeBus.Queues
+{
+    /// <summary>
+    /// All queue messages passed through the queue system must implement this interface.
+    /// </summary>
+    public interface IQueueMessage { }
+}
