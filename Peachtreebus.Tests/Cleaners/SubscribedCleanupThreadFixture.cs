f5dae202cbf42138cf6fb0423fe600675c6778dc
wleader <william.leader@gmail.com>
2022-03-05 11:02:58 -0500

    Create SubscribedCleanupThreadFixture.cs

---
+++ Peachtreebus.Tests/Cleaners/SubscribedCleanupThreadFixture.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using PeachtreeBus;
+using PeachtreeBus.Cleaners;
+using PeachtreeBus.Data;
+using System.Threading.Tasks;
+
+namespace Peachtreebus.Tests.Cleaners
+{
+    /// <summary>
+    /// Proves the behavior of SubscribedCleanupThread
+    /// </summary>
+    [TestClass]
+    public class SubscribedCleanupThreadFixture
+    {
+        private SubscribedCleanupThread thread;
+        private Mock<ILog<SubscribedCleanupThread>> log;
+        private Mock<IBusDataAccess> dataAccess;
+        private Mock<IProvideShutdownSignal> shutdown;
+        private Mock<ISubscribedCleanupWork> cleaner;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            log = new Mock<ILog<SubscribedCleanupThread>>();
+            dataAccess = new Mock<IBusDataAccess>();
+            shutdown = new Mock<IProvideShutdownSignal>();
+            cleaner = new Mock<ISubscribedCleanupWork>();
+
+            thread = new SubscribedCleanupThread(log.Object,
+                dataAccess.Object, shutdown.Object, cleaner.Object);
+        }
+
+        /// <summary>
+        /// Proves the cleaner is invoked.
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task DoUnitOfwork_DoesWork()
+        {
+            await thread.DoUnitOfWork();
+            cleaner.Verify(c => c.DoWork(), Times.Once);
+        }
+    }
+}
