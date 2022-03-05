ebdfa4a9afc17c4d9f7685c0035e37ead5c581fe
wleader <william.leader@gmail.com>
2022-03-05 09:47:48 -0500

    Update SagaCompletedHandler.cs

--- PeachtreeBus.Example/Handlers/SagaCompletedHandler.cs
+++ PeachtreeBus.Example/Handlers/SagaCompletedHandler.cs
@@ -1,24 +1,39 @@
 ﻿using PeachtreeBus.Example.Data;
 using PeachtreeBus.Example.Messages;
+using PeachtreeBus.Queues;
+using PeachtreeBus.Subscriptions;
 using System.Threading.Tasks;
 
 namespace PeachtreeBus.Example.Handlers
 {
-    public class SagaCompletedHandler : IHandleMessage<SampleSagaComplete>
+    /// <summary>
+    /// An example handler for the SampleSagaCompelteMessage
+    /// </summary>
+    public class SagaCompletedHandler : IHandleQueueMessage<SampleSagaComplete>
     {
         private readonly ILog _log;
         private readonly IExampleDataAccess _dataAccess;
+        private readonly ISubscribedPublisher _publisher;
 
-        public SagaCompletedHandler(ILog<SagaCompletedHandler> log, IExampleDataAccess dataAccess)
+        public SagaCompletedHandler(ILog<SagaCompletedHandler> log,
+            IExampleDataAccess dataAccess,
+            ISubscribedPublisher publisher)
         {
             _log = log;
             _dataAccess = dataAccess;
+            _publisher = publisher;
         }
 
-        public Task Handle(MessageContext context, SampleSagaComplete message)
+        public async Task Handle(QueueContext context, SampleSagaComplete message)
         {
             _log.Info("Distributed Saga Complete!");
-            return _dataAccess.Audit("Example Saga Completed.");
+
+            // send an announcement message to all that have subscribed.
+            await _publisher.PublishMessage("Announcements", new AnnounceSagaCompleted
+            {
+                AppId = message.AppId
+            });
+            await _dataAccess.Audit("Example Saga Completed.");
         }
     }
 }
