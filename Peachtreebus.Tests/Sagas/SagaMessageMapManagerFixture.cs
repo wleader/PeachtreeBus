3c450660ced426be9a9becef86fac4449366566b
wleader <william.leader@gmail.com>
2022-03-05 11:27:01 -0500

    Create SagaMessageMapManagerFixture.cs

---
+++ Peachtreebus.Tests/Sagas/SagaMessageMapManagerFixture.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PeachtreeBus.Queues;
+using PeachtreeBus.Sagas;
+using System;
+
+namespace Peachtreebus.Tests.Sagas
+{
+    /// <summary>
+    /// Proves the behavior of SagaMessageMapManager
+    /// </summary>
+    [TestClass]
+    public class SagaMessageMapManagerFixture
+    {
+        private class MockMessage1 : IQueueMessage {
+            public string Key { get; set; }
+        }
+
+        private class MockMessage2 : IQueueMessage
+        {
+            public string Key { get; set; }
+        }
+
+        private class MockSagaData {  }
+
+        private class MockSaga : Saga<MockSagaData>
+        {
+            public int ConfigureMessageKeysCount = 0;
+
+            public override string SagaName => throw new NotImplementedException();
+
+            public override void ConfigureMessageKeys(SagaMessageMap mapper)
+            {
+                ConfigureMessageKeysCount++;
+                mapper.Add<MockMessage1>((m) => m.Key);
+                mapper.Add<MockMessage2>((m) => m.Key);
+            }
+        }
+
+        private SagaMessageMapManager manager;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            manager = new SagaMessageMapManager();
+        }
+
+        /// <summary>
+        /// Proves that the saga is configured only once.
+        /// </summary>
+        [TestMethod]
+        public void GetKey_ConfiguresOnlyOnce_AndUsesProvidedFunctions()
+        {
+            var saga = new MockSaga();
+            var message1 = new MockMessage1() { Key = "FooKey" };
+            var message2 = new MockMessage2() { Key = "BazKey" };
+
+            Assert.AreEqual(0, saga.ConfigureMessageKeysCount);
+
+            var result1 = manager.GetKey(saga, message1);
+            var result2 = manager.GetKey(saga, message2);
+
+            Assert.AreEqual("FooKey", result1);
+            Assert.AreEqual("BazKey", result2);
+            Assert.AreEqual(1, saga.ConfigureMessageKeysCount);
+        }
+    }
+}
