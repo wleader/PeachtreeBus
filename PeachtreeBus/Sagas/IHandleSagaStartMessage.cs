ce0c43b24b34f24ccdb73355ac108d327f06878d
wleader <william.leader@gmail.com>
2022-03-05 12:12:54 -0500

    Create IHandleSagaStartMessage.cs

---
+++ PeachtreeBus/Sagas/IHandleSagaStartMessage.cs
@@ -0,0 +1,13 @@
+﻿using PeachtreeBus.Queues;
+
+namespace PeachtreeBus.Sagas
+{
+    /// <summary>
+    /// Defines an interface for a class that handles a message that is allowed to start a saga.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IHandleSagaStartMessage<T> : IHandleQueueMessage<T> where T : IQueueMessage
+    {
+
+    }
+}
