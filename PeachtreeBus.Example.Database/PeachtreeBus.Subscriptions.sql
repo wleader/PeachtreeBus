e582c3db69025275a0a549cad661f8c1ce324dd2
wleader <william.leader@gmail.com>
2022-03-05 09:46:05 -0500

    Create PeachtreeBus.Subscriptions.sql

---
+++ PeachtreeBus.Example.Database/PeachtreeBus.Subscriptions.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [PeachtreeBus].[Subscriptions]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY,
+	[SubscriberId] UNIQUEIDENTIFIER NOT NULL,
+	[Category] NVARCHAR(128) NOT NULL,
+	[ValidUntil] DATETIME2 NOT NULL,
+	CONSTRAINT AK_SubscriberCategory UNIQUE([SubscriberId], [Category])
+)
