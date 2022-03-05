eec55e04fc3dad9c3188195823ae270e9a7f4621
wleader <william.leader@gmail.com>
2022-03-05 12:08:56 -0500

    Create IHandleQueueMessage.cs

---
+++ PeachtreeBus/Queues/IHandleQueueMessage.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace PeachtreeBus.Queues
+{
+    /// <summary>
+    /// Defines an interface for classes that will handle a message of a given type.
+    /// </summary>
+    /// <typeparam name="T">A message class</typeparam>
+    public interface IHandleQueueMessage<T> where T : IQueueMessage
+    {
+        Task Handle(QueueContext context, T message);
+    }
+}
