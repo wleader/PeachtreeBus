6bb169a0af3bc80c24756c6974247aa2bfd64aaa
wleader <william.leader@gmail.com>
2022-03-05 11:01:15 -0500

    Create QueueCleanupThreadFixture.cs

---
+++ Peachtreebus.Tests/Cleaners/QueueCleanupThreadFixture.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using PeachtreeBus;
+using PeachtreeBus.Cleaners;
+using PeachtreeBus.Data;
+using System.Threading.Tasks;
+
+namespace Peachtreebus.Tests.Cleaners
+{
+    /// <summary>
+    /// Proves the behavior of QueueCleanupThread
+    /// </summary>
+    [TestClass]
+    public class QueueCleanupThreadFixture
+    {
+        private QueueCleanupThread thread;
+        private Mock<ILog<QueueCleanupThread>> log;
+        private Mock<IBusDataAccess> dataAccess;
+        private Mock<IProvideShutdownSignal> shutdown;
+        private Mock<IQueueCleanupWork> cleaner;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            log = new Mock<ILog<QueueCleanupThread>>();
+            dataAccess = new Mock<IBusDataAccess>();
+            shutdown = new Mock<IProvideShutdownSignal>();
+            cleaner = new Mock<IQueueCleanupWork>();
+
+            thread = new QueueCleanupThread(log.Object,
+                dataAccess.Object, shutdown.Object, cleaner.Object);
+        }
+
+        /// <summary>
+        /// Prove the cleaner is invoked.
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task DoUnitOfwork_DoesWork()
+        {
+            await thread.DoUnitOfWork();
+            cleaner.Verify(c => c.DoWork(), Times.Once);
+        }
+    }
+}
