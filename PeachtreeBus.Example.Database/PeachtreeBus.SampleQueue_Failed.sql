f44d389f88210d66f49679ac5ffe85f02f1b9532
wleader <william.leader@gmail.com>
2022-03-05 09:45:30 -0500

    Create PeachtreeBus.SampleQueue_Failed.sql

---
+++ PeachtreeBus.Example.Database/PeachtreeBus.SampleQueue_Failed.sql
@@ -0,0 +1,17 @@
+﻿-- A table for failed messages from the queue named SampleQueue
+CREATE TABLE [PeachtreeBus].[SampleQueue_Failed]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY, 
+    [MessageId] UNIQUEIDENTIFIER NOT NULL, 
+    [NotBefore] DATETIME2 NOT NULL, 
+    [Enqueued] DATETIME2 NOT NULL,
+    [Completed] DATETIME2 NULL,
+    [Failed] DATETIME2 NULL, 
+    [Retries] TINYINT NOT NULL,
+    [Headers] NVARCHAR(MAX) NOT NULL,
+    [Body] NVARCHAR(MAX) NOT NULL
+)
+GO
+
+ALTER TABLE [PeachtreeBus].[SampleQueue_Failed] ADD DEFAULT ((0)) FOR [Retries]
+GO
