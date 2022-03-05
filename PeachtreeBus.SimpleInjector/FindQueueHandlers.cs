805bfdd777a83b0b5be0bfc6b8d8593674cfa9f7
wleader <william.leader@gmail.com>
2022-03-05 10:24:26 -0500

    Create FindQueueHandlers.cs

---
+++ PeachtreeBus.SimpleInjector/FindQueueHandlers.cs
@@ -0,0 +1,26 @@
+﻿using PeachtreeBus.Queues;
+using System.Collections.Generic;
+
+namespace PeachtreeBus.SimpleInjector
+{
+    /// <summary>
+    /// an implementation of IFindMessageHandlers that gets the handlers from a SimpleInjector container.
+    /// </summary>
+    public class FindQueueHandlers : IFindQueueHandlers
+    {
+        private readonly IWrappedScope _scope;
+        public FindQueueHandlers(IWrappedScope scope)
+        {
+            _scope = scope;
+        }
+
+        public IEnumerable<IHandleQueueMessage<T>> FindHandlers<T>() where T : IQueueMessage
+        {
+            // create the handlers in the current scope.
+            // this is because when multiple threads are handling messages, each thread needs 
+            // to have its own instance of the handler object, so that mulitple threads do not
+            // intefere with each other.
+            return _scope.GetAllInstances<IHandleQueueMessage<T>>();
+        }
+    }
+}
