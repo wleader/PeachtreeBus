5d0fee699abb739ff1e17ea8151410f449b3975b
wleader <william.leader@gmail.com>
2022-03-05 09:39:41 -0500

    Create SubscriptionRow.cs

---
+++ PeachtreeBus.DataAccessTests/SubscriptionRow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PeachtreeBus.DataAccessTests
+{
+    /// <summary>
+    /// A class for testing the content of the subscriptions table.
+    /// The main project model doesn't need this so its only in the test code.
+    /// </summary>
+    public class SubscriptionsRow
+    {
+        public virtual long Id { get; set; }
+        public virtual Guid SubscriberId { get; set; }
+        public virtual string Category { get; set; }
+        public virtual DateTime ValidUntil { get; set; }
+    }
+}
