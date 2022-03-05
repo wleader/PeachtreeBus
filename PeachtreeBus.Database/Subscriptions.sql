6862c0a29af3e7e06078934d6b724c4c4a310708
wleader <william.leader@gmail.com>
2022-03-05 09:45:02 -0500

    Create Subscriptions.sql

---
+++ PeachtreeBus.Database/Subscriptions.sql
@@ -0,0 +1,10 @@
+﻿-- Reference definition for the Subscriptions table.
+
+CREATE TABLE [PeachtreeBus].[Subscriptions]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY IDENTITY,
+	[SubscriberId] UNIQUEIDENTIFIER NOT NULL,
+	[Category] NVARCHAR(128) NOT NULL,
+	[ValidUntil] DATETIME2 NOT NULL,
+	CONSTRAINT AK_SubscriberCategory UNIQUE([SubscriberID], [Category])
+)
