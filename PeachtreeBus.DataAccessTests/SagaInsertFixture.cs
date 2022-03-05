74303ada85e929b7788750a7187107ce8ab466f1
wleader <william.leader@gmail.com>
2022-03-05 09:14:14 -0500

    Create SagaInsertFixture.cs

---
+++ PeachtreeBus.DataAccessTests/SagaInsertFixture.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+
+namespace PeachtreeBus.DataAccessTests
+{
+    /// <summary>
+    /// Proves the behavior of DapperDataAccess.Insert saga
+    /// </summary>
+    [TestClass]
+    public class SagaInsertFixture : FixtureBase
+    {
+        [TestInitialize]
+        public override void TestInitialize()
+        {
+            base.TestInitialize();
+        }
+
+        [TestCleanup]
+        public override void TestCleanup()
+        {
+            base.TestCleanup();
+        }
+
+        /// <summary>
+        /// Proves the data is inserted correctly.
+        /// </summary>
+        /// <returns></returns>
+        [TestMethod]
+        public async Task InsertSaga_StoresTheData()
+        {
+            var newSaga = CreateTestSagaData();
+
+            Assert.AreEqual(0, CountRowsInTable(DefaultSagaTable));
+
+            newSaga.Id = await dataAccess.Insert(newSaga, DefaultSagaName);
+
+            Assert.IsTrue(newSaga.Id > 0);
+
+            var data = GetTableContent(DefaultSagaTable);
+            Assert.IsNotNull(data);
+
+            var sagas = data.ToSagas();
+            Assert.AreEqual(1, sagas.Count);
+
+            AssertSagaEquals(newSaga, sagas[0]);
+        }
+
+        /// <summary>
+        /// proves that unsafe schema are not allowed.
+        /// </summary>
+        [TestMethod]
+        public void InsertSaga_ThrowsIfSchemaContainsUnsafe()
+        {
+            var action = new Action(() => dataAccess.Insert(new Model.SagaData(), DefaultSagaName));
+            ActionThrowsIfSchemaContainsPoisonChars(action);
+        }
+
+        /// <summary>
+        /// proves that unsafe schema names are not allowed.
+        /// </summary>
+        [TestMethod]
+        public void InsertSaga_ThrowsIfSagaNameContainsUnsafe()
+        {
+            var action = new Action<string>((s) => dataAccess.Insert(new Model.SagaData(), s));
+            ActionThrowsIfParameterContainsPoisonChars(action);
+        }
+    }
+}
