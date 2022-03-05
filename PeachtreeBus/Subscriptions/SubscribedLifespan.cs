c42b8420aec254786d428f090551bb62116717da
wleader <william.leader@gmail.com>
2022-03-05 12:16:29 -0500

    Create SubscribedLifespan.cs

---
+++ PeachtreeBus/Subscriptions/SubscribedLifespan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PeachtreeBus.Subscriptions
+{
+    /// <summary>
+    /// defines how long a subscribed message is valid for, before 
+    /// it will be discarded by the system if the subscriber does not consume it.
+    /// Configuration information for SubscriptionPublisher
+    /// </summary>
+    public interface ISubscribedLifespan
+    {
+        TimeSpan Duration { get; }
+    }
+
+    /// <summary>
+    /// Default implmentation of ISubscribedLifespan.
+    /// </summary>
+    public class SubscribedLifespan : ISubscribedLifespan
+    {
+        public TimeSpan Duration { get; private set; }
+
+        public SubscribedLifespan(TimeSpan duration)
+        {
+            Duration = duration;
+        }
+    }
+}
