f1883f2159f996dc38b26cdd6b235def05353013
wleader <william.leader@gmail.com>
2022-03-05 11:53:43 -0500

    Create SubscribedCleanupConfiguration.cs

---
+++ PeachtreeBus/Cleaners/SubscribedCleanupConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PeachtreeBus.Cleaners
+{
+    /// <summary>
+    /// Provides configuration to the Subscribed Cleaner
+    /// </summary>
+    public interface ISubscribedCleanupConfiguration : IBaseCleanupConfiguration { }
+
+    /// <summary>
+    /// A default implementation of ISubscribedCleanupConfiguration
+    /// </summary>
+    public class SubscribedCleanupConfiguration : BaseCleanupConfiguration, ISubscribedCleanupConfiguration
+    {
+        public SubscribedCleanupConfiguration(int maxDeleteCount, bool cleanCompleted, bool cleanFailed,
+            TimeSpan ageLimit, TimeSpan interval)
+            : base(maxDeleteCount, cleanCompleted, cleanFailed, ageLimit, interval)
+        { }
+    }
+}
