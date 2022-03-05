6f59fbb35e86a45c9b8c0f9642deb821ae7e5f27
wleader <william.leader@gmail.com>
2022-03-05 12:14:35 -0500

    Create IFindSubscribedHandlers.cs

---
+++ PeachtreeBus/Subscriptions/IFindSubscribedHandlers.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace PeachtreeBus.Subscriptions
+{
+    public interface IFindSubscribedHandlers
+    {
+        /// <summary>
+        /// Gets an enumerable of Message hander classes from the Dependency Injection container.
+        /// </summary>
+        /// <typeparam name="T">The Type of the message.</typeparam>
+        /// <returns>An Enumberable of message handlers for the given message type.</returns>
+        IEnumerable<IHandleSubscribedMessage<T>> FindHandlers<T>() where T : ISubscribedMessage;
+    }
+}
