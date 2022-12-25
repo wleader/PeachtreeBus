e67f8925a438183142f9cca82229762d4c072031
wleader <william.leader@gmail.com>
2022-12-25 11:06:39 -0500

    Create FindQueuedPipelineSteps.cs

---
+++ PeachtreeBus.SimpleInjector/FindQueuedPipelineSteps.cs
@@ -0,0 +1,24 @@
+﻿using PeachtreeBus.Queues;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PeachtreeBus.SimpleInjector
+{
+    /// <summary>
+    /// An Implementation of IFindQueuePipelineSteps using SimpleInjector.
+    /// </summary>
+    public class FindQueuedPipelineSteps : IFindQueuePipelineSteps
+    {
+        private readonly IWrappedScope _scope;
+
+        public FindQueuedPipelineSteps(IWrappedScope scope)
+        {
+            _scope = scope;
+        }
+
+        public IEnumerable<IQueuePipelineStep> FindSteps()
+        {
+            return _scope.GetAllInstances<IQueuePipelineStep>().ToList();
+        }
+    }
+}
