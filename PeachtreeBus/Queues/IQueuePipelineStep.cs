8f9a331c13cbe078e4cdf4cfccf58af0ab09e8b3
wleader <william.leader@gmail.com>
2022-12-25 11:11:52 -0500

    Create IQueuePipelineStep.cs

---
+++ PeachtreeBus/Queues/IQueuePipelineStep.cs
@@ -0,0 +1,9 @@
+﻿using PeachtreeBus.Pipelines;
+
+namespace PeachtreeBus.Queues
+{
+    public interface IQueuePipelineStep : IPipelineStep<QueueContext>
+    {
+        public int Priority { get; }
+    }
+}
