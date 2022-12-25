500d4b791f3efa00b6a40cb8b8e4c51a6b9fb04d
wleader <william.leader@gmail.com>
2022-12-25 11:08:04 -0500

    Update UsePeachtreeBusSubscriptions.cs

--- PeachtreeBus.SimpleInjector/UsePeachtreeBusSubscriptions.cs
+++ PeachtreeBus.SimpleInjector/UsePeachtreeBusSubscriptions.cs
@@ -21,11 +21,16 @@ namespace PeachtreeBus.SimpleInjector
             container.Register(typeof(ISubscribedWork), typeof(SubscribedWork), Lifestyle.Scoped);
             container.Register(typeof(ISubscribedReader), typeof(SubscribedReader), Lifestyle.Scoped);
             container.Register(typeof(IFindSubscribedHandlers), typeof(FindSubscribedHandlers), Lifestyle.Scoped);
+            container.Register(typeof(IFindSubscribedPipelineSteps), typeof(FindSubscribedPipelineSteps), Lifestyle.Scoped);
             container.Register(typeof(ISubscriptionUpdateThread), typeof(SubscriptionUpdateThread), Lifestyle.Scoped);
             container.Register(typeof(ISubscriptionUpdateWork), typeof(SubscriptionUpdateWork), Lifestyle.Scoped);
 
             // register our subscription configuration.
             container.Register(typeof(ISubscriberConfiguration), () => config, Lifestyle.Singleton);
+
+            // register pipeline steps
+            container.RegisterPeachtreeBusSubscribedPipelineSteps();
+
             return container;
         }
     }
