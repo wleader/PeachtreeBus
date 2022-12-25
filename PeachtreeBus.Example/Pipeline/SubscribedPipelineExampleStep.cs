952d36c7f56d9cf554558a4bbe907d3da3840d04
wleader <william.leader@gmail.com>
2022-12-25 11:06:08 -0500

    Create SubscribedPipelineExampleStep.cs

---
+++ PeachtreeBus.Example/Pipeline/SubscribedPipelineExampleStep.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using PeachtreeBus.Subscriptions;
+using System;
+using System.Threading.Tasks;
+
+namespace PeachtreeBus.Example.Pipeline
+{
+    internal class SubscribedPipelineExampleStep : ISubscribedPipelineStep
+    {
+        private readonly ILogger<SubscribedPipelineExampleStep> _log;
+
+        public SubscribedPipelineExampleStep(ILogger<SubscribedPipelineExampleStep> log)
+        {
+            _log = log;
+        }
+
+        public int Priority => 1;
+
+        public async Task Invoke(SubscribedContext context, Func<SubscribedContext, Task> next)
+        {
+            _log.LogInformation("This code runs before Subscribed Message handlers.");
+
+            await next(context);
+
+            _log.LogInformation("This code runs after Subscribed Message handlers.");
+        }
+    }
+}
