75e966f7de0fe40e0e034c350e99e12abb5042fc
wleader <william.leader@gmail.com>
2022-12-25 11:11:41 -0500

    Create PipelineLink.cs

---
+++ PeachtreeBus/Pipelines/PipelineLink.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PeachtreeBus.Pipelines
+{
+    internal class PipelineLink<TContext>
+    {
+        private PipelineLink<TContext> _next;
+        private readonly IPipelineStep<TContext> _step;
+
+        public PipelineLink(IPipelineStep<TContext> step)
+        {
+            _step = step;
+        }
+
+        public void SetNext(PipelineLink<TContext> next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(TContext context)
+        {
+            Func<TContext, Task> stepParam =
+                _next is null
+                ? NullNext
+                : _next.Invoke;
+            await _step.Invoke(context, stepParam);
+        }
+
+        private static Task NullNext(TContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
