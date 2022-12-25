24ff56664ff0bb7c4cf562b4af98380f595fd09f
wleader <william.leader@gmail.com>
2022-12-25 11:05:59 -0500

    Create QueuePipelineExampleStep.cs

---
+++ PeachtreeBus.Example/Pipeline/QueuePipelineExampleStep.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using PeachtreeBus.Queues;
+using System;
+using System.Threading.Tasks;
+
+namespace PeachtreeBus.Example.Pipeline
+{
+    internal class QueuePipelineExampleStep : IQueuePipelineStep
+    {
+        private readonly ILogger<QueuePipelineExampleStep> _log;
+
+        public QueuePipelineExampleStep(ILogger<QueuePipelineExampleStep> log)
+        {
+            _log = log;
+        }
+
+        public int Priority => 1;
+
+        public async Task Invoke(QueueContext context, Func<QueueContext, Task> next)
+        {
+            _log.LogInformation("This code runs before Queue Message handlers.");
+
+            await next(context);
+
+            _log.LogInformation("This code runs after Queue Message handlers.");
+        }
+    }
+}
