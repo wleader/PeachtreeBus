f5c23deb01c57865abdc832801fe7235dc84f424
wleader <william.leader@gmail.com>
2022-12-25 11:07:42 -0500

    Update SimpleInjectorScopeFactoryException.cs

--- PeachtreeBus.SimpleInjector/SimpleInjectorScopeFactoryException.cs
+++ PeachtreeBus.SimpleInjector/SimpleInjectorScopeFactoryException.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace PeachtreeBus.SimpleInjector
 {
-    public class SimpleInjectorScopeFactoryException :Exception
+    public class SimpleInjectorScopeFactoryException : Exception
     {
         internal SimpleInjectorScopeFactoryException(string message)
             : base(message)
