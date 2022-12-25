1cd53d0c89c31ab3451d5fb0c5a041bab9bab9cc
wleader <william.leader@gmail.com>
2022-12-25 11:12:47 -0500

    Update SubscribedReader.cs

--- PeachtreeBus/Subscriptions/SubscribedReader.cs
+++ PeachtreeBus/Subscriptions/SubscribedReader.cs
@@ -22,67 +22,6 @@ namespace PeachtreeBus.Subscriptions
         Task Fail(SubscribedContext subsriptionContext, Exception ex);
     }
 
-    internal static class SubscribedReader_LogMessages
-    {
-        internal static Action<ILogger, Guid, Guid, int, Exception> SubscribedReader_MessageExceededMaxRetries_Action =
-            LoggerMessage.Define<Guid, Guid, int>(
-                LogLevel.Error,
-                Events.SubscribedReader_MessageExceededMaxRetries,
-                "Message {MessageId} for Subscriber {SubscriberId} execeed the max number of retries ({MaxRetries}) and has failed.");
-
-        internal static void SubscribedReader_MessageExceededMaxRetries(this ILogger logger,
-            Guid messageId, Guid subscriberId, int maxRetries)
-        {
-            SubscribedReader_MessageExceededMaxRetries_Action(logger, messageId, subscriberId, maxRetries, null);
-        }
-
-        internal static Action<ILogger, Guid, Guid, Exception> SubscribedReader_HeaderNotDeserializable_Action =
-            LoggerMessage.Define<Guid, Guid>(
-                LogLevel.Warning,
-                Events.SubscribedReader_HeaderNotDeserializable,
-                "Headers could not be deserialized for message {MessageId} for subscriber {SubscriberId}");
-
-        internal static void SubscribedReader_HeaderNotDeserializable(this ILogger logger,
-            Guid messageId, Guid subscriberId)
-        {
-            SubscribedReader_HeaderNotDeserializable_Action(logger, messageId, subscriberId, null);
-        }
-
-        internal static Action<ILogger, Guid, Guid, Exception> SubscribedReader_BodyNotDeserializable_Action =
-            LoggerMessage.Define<Guid, Guid>(
-                LogLevel.Warning,
-                Events.SubscribedReader_BodyNotDeserializable,
-                "Message Body could not be deserialized for message {MessageId} for subscriber {SubscriberId}.");
-
-        internal static void SubscribedReader_BodyNotDeserializable(this ILogger logger,
-            Guid messageId, Guid subscriberId, Exception ex)
-        {
-            SubscribedReader_HeaderNotDeserializable_Action(logger, messageId, subscriberId, ex);
-        }
-
-        internal static readonly Action<ILogger, string, Guid, Guid, Exception> SubscribedReader_MessageClassNotRecognized_Action =
-            LoggerMessage.Define<string, Guid, Guid>(
-                LogLevel.Warning,
-                Events.SubscribedReader_MessageClassNotRecognized,
-                "Message class '{MessageClass}' was not recognized for message {MessageId} for subscriber {SubscriberId}");
-
-        internal static void SubscribedReader_MessageClassNotRecognized(this ILogger logger, Guid messageId, Guid subscriberId, string messageClass)
-        {
-            SubscribedReader_MessageClassNotRecognized_Action(logger, messageClass, messageId, subscriberId, null);
-        }
-
-        internal static readonly Action<ILogger, Guid, Guid, DateTime, Exception> SubscribedReader_MessageWillBeRetried_Action =
-            LoggerMessage.Define<Guid, Guid, DateTime>(
-                LogLevel.Warning,
-                Events.SubscribedReader_MessageWillBeRetried,
-                "Message {MessageId} from queue {QueueName} will be retried after {NotBefore}.");
-
-        internal static void SubscribedReader_MessageWillBeRetried(this ILogger logger, Guid messageId, Guid subscriberId, DateTime notBefore)
-        {
-            SubscribedReader_MessageWillBeRetried_Action(logger, messageId, subscriberId, notBefore, null);
-        }
-    }
-
     /// <summary>
     /// Reads and updates, completes, and fails subscribed messages.
     /// </summary>
@@ -167,9 +106,9 @@ namespace PeachtreeBus.Subscriptions
             {
                 headers = _serializer.DeserializeHeaders(subscriptionMessage.Headers);
             }
-            catch
+            catch (Exception ex)
             {
-                _log.SubscribedReader_HeaderNotDeserializable(subscriptionMessage.MessageId, subscriberId);
+                _log.SubscribedReader_HeaderNotDeserializable(subscriptionMessage.MessageId, subscriberId, ex);
                 // this might not work, The body might deserialize but there won't be an
                 // IHandleMessages<System.Object> so it won't get handled. This really just gives
                 // us a chance to get farther and log more about the bad message.
@@ -193,7 +132,7 @@ namespace PeachtreeBus.Subscriptions
             }
             else
             {
-                _log.SubscribedReader_MessageClassNotRecognized(subscriptionMessage.MessageId, subscriberId, headers.MessageClass);
+                _log.SubscribedReader_MessageClassNotRecognized(headers.MessageClass, subscriptionMessage.MessageId, subscriberId);
             }
 
             // return the new message context.
