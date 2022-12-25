22c604a5ad177a0a3fe68cf516ca083a66af2538
wleader <william.leader@gmail.com>
2022-12-25 11:12:33 -0500

    Create IFindSubscribedPipelineSteps.cs

---
+++ PeachtreeBus/Subscriptions/IFindSubscribedPipelineSteps.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace PeachtreeBus.Subscriptions
+{
+    public interface IFindSubscribedPipelineSteps
+    {
+        IEnumerable<ISubscribedPipelineStep> FindSteps();
+    }
+}
