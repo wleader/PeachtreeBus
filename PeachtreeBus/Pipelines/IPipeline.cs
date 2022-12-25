4a833b0c022b88674d6691e184becf5c247b0bc6
wleader <william.leader@gmail.com>
2022-12-25 11:11:28 -0500

    Create IPipeline.cs

---
+++ PeachtreeBus/Pipelines/IPipeline.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace PeachtreeBus.Pipelines
+{
+    public interface IPipeline<TContext>
+    {
+        void Add(IPipelineStep<TContext> steps);
+        Task Invoke(TContext context);
+    }
+}
