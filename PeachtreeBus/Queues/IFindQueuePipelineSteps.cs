46e0fee49c0edb44c5dd37f08fb325bdda34aa78
wleader <william.leader@gmail.com>
2022-12-25 11:11:48 -0500

    Create IFindQueuePipelineSteps.cs

---
+++ PeachtreeBus/Queues/IFindQueuePipelineSteps.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace PeachtreeBus.Queues
+{
+    public interface IFindQueuePipelineSteps
+    {
+        IEnumerable<IQueuePipelineStep> FindSteps();
+    }
+}
