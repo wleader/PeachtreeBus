c990773b66b7eb88bd25a71b0c26be7e4fe81c6a
wleader <william.leader@gmail.com>
2022-12-25 11:11:37 -0500

    Create Pipeline.cs

---
+++ PeachtreeBus/Pipelines/Pipeline.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+
+namespace PeachtreeBus.Pipelines
+{
+    public class Pipeline<TContext> : IPipeline<TContext>
+    {
+        PipelineLink<TContext> _first;
+        PipelineLink<TContext> _last;
+
+        public void Add(IPipelineStep<TContext> step)
+        {
+            var link = new PipelineLink<TContext>(step);
+            _first ??= link;
+            _last?.SetNext(link);
+            _last = link;
+        }
+
+        public Task Invoke(TContext context)
+        {
+            return _first?.Invoke(context);
+        }
+    }
+}
