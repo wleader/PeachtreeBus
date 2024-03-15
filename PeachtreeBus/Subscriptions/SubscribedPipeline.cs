f12f47a23a63809b7f6b75b1224f17a889e65531
wleader <william.leader@gmail.com>
2024-03-15 16:13:30 -0400

    Define classes/interfaces that are pipelines for Subscribed message handling.

---
+++ PeachtreeBus/Subscriptions/SubscribedPipeline.cs
@@ -0,0 +1,7 @@
+﻿using PeachtreeBus.Pipelines;
+
+namespace PeachtreeBus.Subscriptions
+{
+    public interface ISubscribedPipeline : IPipeline<SubscribedContext> { }
+    public class SubscribedPipeline : Pipeline<SubscribedContext>, ISubscribedPipeline { }
+}
