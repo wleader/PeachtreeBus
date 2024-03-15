ae0e2ad973829efba44395ef3a398c0d3b5766ab
wleader <william.leaer@gmail.com>
2024-03-15 12:44:24 -0400

    Define classes/interfaces that are pipelines for Queued message handling.

---
+++ PeachtreeBus/Queues/QueuePipeline.cs
@@ -0,0 +1,8 @@
+﻿using PeachtreeBus.Pipelines;
+
+namespace PeachtreeBus.Queues
+{
+    public interface IQueuePipeline : IPipeline<QueueContext> { }
+
+    public class QueuePipeline : Pipeline<QueueContext>, IQueuePipeline { }
+}
