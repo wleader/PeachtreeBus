712bf840e8980982628c7d04d7919bd4b4a5e7dc
wleader <william.leaer@gmail.com>
2024-03-15 12:47:46 -0400

    Moving the code that builds a Queue Pipeline into its own testable class.

---
+++ PeachtreeBus/Queues/QueuePipelineFactory.cs
@@ -0,0 +1,48 @@
+﻿using PeachtreeBus.Pipelines;
+using System.Linq;
+
+namespace PeachtreeBus.Queues
+{
+    /// <summary>
+    /// Builds a pipeline for handling a Queued message.
+    /// </summary>
+    public interface IQueuePipelineFactory
+    {
+        IQueuePipeline Build();
+    }
+
+    /// <summary>
+    /// Builds a pipeline for handling a Queued message.
+    /// </summary>
+    public class QueuePipelineFactory(
+        IWrappedScope scope)
+        : IQueuePipelineFactory
+    {
+        private readonly IWrappedScope _scope = scope;
+
+        public IQueuePipeline Build()
+        {
+            // create a pipeline.
+            var result = _scope.GetInstance<IQueuePipeline>();
+
+            // get the steps that should be in the pipeline.
+            var findSteps = _scope.GetInstance<IFindQueuePipelineSteps>();
+            // order the pipeline steps by priority.
+            var steps = findSteps.FindSteps().OrderBy(s => s.Priority);
+
+            // add the steps to the pipeline.
+            foreach (var step in steps)
+            {
+                result.Add(step);
+            }
+
+            // the very last step in the chain is to pass the message to the
+            // handlers.
+            var handlersStep = _scope.GetInstance<IQueueHandlersPipelineStep>();
+            result.Add(handlersStep);
+
+            // Pipeline is ready.
+            return result;
+        }
+    }
+}
---
+++ Peachtreebus.Tests/Pipeline/QueuePipelineFactoryFixture.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using PeachtreeBus;
+using PeachtreeBus.Queues;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Peachtreebus.Tests.Pipeline
+{
+    [TestClass]
+    public class QueuePipelineFactoryFixture
+    {
+        private Mock<IQueuePipeline> _pipeline;
+        private Mock<IFindQueuePipelineSteps> _findSteps;
+        private Mock<IWrappedScope> _scope;
+        private Mock<IQueueHandlersPipelineStep> _handlersStep;
+        private QueuePipelineFactory _factory;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _pipeline = new();
+            _findSteps = new();
+            _scope = new();
+            _handlersStep = new();
+
+            _scope.Setup(s => s.GetInstance<IQueuePipeline>()).Returns(_pipeline.Object);
+            _scope.Setup(s => s.GetInstance<IFindQueuePipelineSteps>()).Returns(_findSteps.Object);
+            _scope.Setup(s => s.GetInstance<IQueueHandlersPipelineStep>()).Returns(_handlersStep.Object);
+
+            _factory = new QueuePipelineFactory(_scope.Object);
+        }
+
+        [TestMethod]
+        public void Given_NoPipelineStepsAreProvided_When_Build_Then_OnlyHandlerStepIsAdded()
+        {
+
+            _findSteps.Setup(f => f.FindSteps()).Returns(Array.Empty<IQueuePipelineStep>());
+
+            var result = _factory.Build();
+
+            Assert.IsTrue(ReferenceEquals(result, _pipeline.Object));
+
+            Assert.AreEqual(1, _pipeline.Invocations.Count);
+            _pipeline.Verify(p => p.Add(_handlersStep.Object), Times.Once);
+        }
+
+        [TestMethod]
+        public void Given_PipelineStepsAreProvided_When_Build_Then_StepsAreAdded()
+        {
+            var step1 = new Mock<IQueuePipelineStep>();
+            var step2 = new Mock<IQueuePipelineStep>();
+            var step3 = new Mock<IQueuePipelineStep>();
+
+            // note that priorities are out of order.
+            step1.SetupGet(s => s.Priority).Returns(3);
+            step2.SetupGet(s => s.Priority).Returns(2);
+            step3.SetupGet(s => s.Priority).Returns(1);
+
+            List<IQueuePipelineStep> steps = [step1.Object, step2.Object, step3.Object];
+            _findSteps.Setup(f => f.FindSteps()).Returns(steps);
+
+            var result = _factory.Build();
+
+            Assert.IsTrue(ReferenceEquals(result, _pipeline.Object));
+
+            // expect 3 steps plus one handler step.
+            Assert.AreEqual(4, _pipeline.Invocations.Count);
+
+            // check that the chain got built in priority order
+            Assert.IsTrue(ReferenceEquals(step3.Object, _pipeline.Invocations[0].Arguments[0]));
+            Assert.IsTrue(ReferenceEquals(step2.Object, _pipeline.Invocations[1].Arguments[0]));
+            Assert.IsTrue(ReferenceEquals(step1.Object, _pipeline.Invocations[2].Arguments[0]));
+
+            // handler must be last in the chain.
+            Assert.IsTrue(ReferenceEquals(_handlersStep.Object, _pipeline.Invocations[_pipeline.Invocations.Count - 1].Arguments[0]));
+        }
+    }
+}
