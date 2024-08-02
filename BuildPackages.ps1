9e845e8933d721af7bb0e04fba0b8f770009cd8e
wleader <william.leader@gmail.com>
2024-08-01 20:06:44 -0400

    Tweaks to Package building to skip projects that don't get included in the nuget pack output.

--- BuildPackages.ps1
+++ BuildPackages.ps1
@@ -3,7 +3,7 @@ Remove-Item -Path .\PeachtreeBus.SimpleInjector\bin\Release -Recurse
 .\nuget.exe restore PeachtreeBus.sln
 
 #this throws erros on the .sqlproj projects, but the other stuff does build. 
-dotnet build PeachtreeBus.sln -p Configuration=Release
+dotnet build PeachtreeBus.sln -p Configuration=Nuget
 
 .\nuget.exe pack PeachtreeBus.nuspec -OutputDirectory .\Packages -Properties Configuration=Release
 .\nuget.exe pack PeachtreeBus.SimpleInjector.nuspec -OutputDirectory .\Packages -Properties Configuration=Release
