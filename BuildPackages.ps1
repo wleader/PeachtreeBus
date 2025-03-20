8ed3df180957bf15d99e3b3863eef65322e6642c
wleader <william.leader@gmail.com>
2025-03-20 19:14:52 -0400

    Makes an .Abstractions Package

--- BuildPackages.ps1
+++ BuildPackages.ps1
@@ -14,12 +14,14 @@ function Quiet-Remove-Folder
 Quiet-Remove-Folder .\PeachtreeBus\bin\Release
 Quiet-Remove-Folder .\PeachtreeBus.SimpleInjector\bin\Release
 Quiet-Remove-Folder .\PeachtreeBus.EntityFrameworkCore\bin\Release
+Quiet-Remove-Folder .\PeachtreeBus.Abstractions\bin\Release
 
 .\nuget.exe restore PeachtreeBus.sln
 
 #this throws erros on the .sqlproj projects, but the other stuff does build. 
 dotnet build PeachtreeBus.sln -p Configuration=Nuget
 
+.\nuget.exe pack PeachtreeBus.Abstractions.nuspec -OutputDirectory .\Packages -Properties Configuration=Release
 .\nuget.exe pack PeachtreeBus.nuspec -OutputDirectory .\Packages -Properties Configuration=Release
 .\nuget.exe pack PeachtreeBus.SimpleInjector.nuspec -OutputDirectory .\Packages -Properties Configuration=Release
 .\nuget.exe pack PeachtreeBus.EntityFrameworkCore.nuspec -OutputDirectory .\Packages -Properties Configuration=Release
