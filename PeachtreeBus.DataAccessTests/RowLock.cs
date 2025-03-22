adae7451c7ea9e2094915b6965cf2c8231104391
wleader <william.leader@gmail.com>
2025-03-21 21:48:47 -0400

    Add a class that connects to SQL and locks rows and then unlocks when disposed.

---
+++ PeachtreeBus.DataAccessTests/RowLock.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Data.SqlClient;
+using PeachtreeBus.Data;
+using PeachtreeBus.DatabaseSharing;
+using System;
+using System.Data;
+
+namespace PeachtreeBus.DataAccessTests;
+
+/// <summary>
+/// A convenience class that locks rows in a table.
+/// IDisposable, so use with a using statement to have it
+/// unlock when the using block ends.
+/// </summary>
+public class RowLock : IDisposable
+{
+    private readonly SqlConnectionProxy _connection;
+    private readonly ISqlTransaction _transaction;
+
+    public DataSet DataSet { get; }
+
+    public RowLock(SchemaName schemaName, TableName tableName, int count = int.MaxValue)
+    {
+        _connection = new SqlConnectionProxy(AssemblyInitialize.DbConnectionString);
+        _connection.Open();
+        _transaction = _connection.BeginTransaction();
+
+        string statement =
+            $"""
+            SELECT TOP (@Count) *
+            FROM [{schemaName}].[{tableName}]
+            WITH (UPDLOCK, READPAST, ROWLOCK)
+            """;
+
+        using var cmd = new SqlCommand(statement, _connection.Connection, _transaction.Transaction);
+        cmd.Parameters.AddWithValue("@Count", count);
+
+        var dataSet = new DataSet();
+        using var adpater = new SqlDataAdapter(cmd);
+        adpater.Fill(dataSet);
+
+        DataSet = dataSet;
+    }
+
+    public void Dispose()
+    {
+        _transaction.Rollback();
+        _transaction.Dispose();
+        _connection.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
