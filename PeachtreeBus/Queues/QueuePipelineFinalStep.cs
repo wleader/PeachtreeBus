4a036dd775cb12d1f1dda2290aee91f56984e54e
wleader <william.leader@gmail.com>
2025-03-25 23:09:41 -0400

    Move the Handler stuff inside the loop into a method to make measuring each activity easier.

--- PeachtreeBus/Queues/QueuePipelineFinalStep.cs
+++ PeachtreeBus/Queues/QueuePipelineFinalStep.cs
@@ -62,59 +62,7 @@ namespace PeachtreeBus.Queues
             foreach (var handler in castHandlers)
             {
                 handlerCount++;
-
-                // determine if this handler is a saga.
-                var handlerType = handler.GetType();
-                InternalContext.CurrentHandler = handlerType.FullName ?? handlerType.ToString();
-
-                var handlerIsSaga = handlerType.IsSubclassOfSaga();
-                if (handlerIsSaga)
-                {
-                    InternalContext.SagaKey = _sagaMessageMapManager.GetKey(handler, context.Message);
-                    _log.QueueWork_LoadingSaga(InternalContext.CurrentHandler, context.SagaKey);
-
-                    await _queueReader.LoadSaga(handler, InternalContext);
-
-                    // if the saga is blocked. Stop.
-                    if (InternalContext.SagaBlocked) return;
-
-                    if (InternalContext.SagaData == null && !handlerType.IsSagaStartHandler(messageType))
-                    {
-                        // the saga was not locked, and it doesn't exist, and this message doesn't start a saga.
-                        // we are processing a saga message but it is not a saga start message and we didnt read previous
-                        // saga data from the DB. This means we are processing a non-start messge before the saga is started.
-                        // we could continute but that would mean that the saga handler might not know it needs to initialize
-                        // the saga data, so its better to stop and make things get fixed.
-                        throw new SagaNotStartedException(context.MessageId,
-                            context.SourceQueue,
-                            messageType,
-                            handlerType,
-                            context.SagaKey);
-                    }
-                }
-
-                // find the right method on the handler.
-                Type[] parameterTypes = [typeof(IQueueContext), messageType];
-                var handleMethod = UnreachableException.ThrowIfNull(handler.GetType().GetMethod("Handle", parameterTypes),
-                    message: "IHandleQueueMessage<> must have a Handle method.");
-
-                // Invoke and await the method.
-                // should it have a seperate try-catch around this and treat it differently?
-                // that would allow us to tell the difference between a problem in a handler, or if the problem was in the bus code.
-                // does that mater for the retry?
-                _log.QueueWork_InvokeHandler(context.MessageId, context.MessageClass, InternalContext.CurrentHandler);
-                {
-                    var taskObject = handleMethod.Invoke(handler, [context, context.Message]);
-                    var castTask = UnreachableException.ThrowIfNull(taskObject as Task,
-                        message: "IHandleQueueMessage<>.Handle must return a not null Task.");
-                    await castTask;
-                }
-
-                if (handlerIsSaga)
-                {
-                    await _queueReader.SaveSaga(handler, InternalContext);
-                    _log.QueueWork_SagaSaved(InternalContext.CurrentHandler, context.SagaKey!);
-                }
+                await InvokeHandler(context, messageType, handler);
             }
 
             if (handlerCount < 1)
@@ -125,6 +73,66 @@ namespace PeachtreeBus.Queues
                     context.MessageId,
                     context.SourceQueue,
                     messageType);
+            }
+        }
+
+        private async Task InvokeHandler(IQueueContext context, Type messageType, object handler)
+        {
+
+            // determine if this handler is a saga.
+            var handlerType = handler.GetType();
+            InternalContext.CurrentHandler = handlerType.FullName ?? handlerType.ToString();
+
+            var handlerIsSaga = handlerType.IsSubclassOfSaga();
+            if (handlerIsSaga)
+            {
+                InternalContext.SagaKey = _sagaMessageMapManager.GetKey(handler, context.Message);
+                _log.QueueWork_LoadingSaga(InternalContext.CurrentHandler, context.SagaKey);
+
+                await _queueReader.LoadSaga(handler, InternalContext);
+
+                // if the saga is blocked. Stop.
+                if (InternalContext.SagaBlocked)
+                {
+                    return;
+                }
+
+                if (InternalContext.SagaData == null && !handlerType.IsSagaStartHandler(messageType))
+                {
+                    // the saga was not locked, and it doesn't exist, and this message doesn't start a saga.
+                    // we are processing a saga message but it is not a saga start message and we didnt read previous
+                    // saga data from the DB. This means we are processing a non-start messge before the saga is started.
+                    // we could continute but that would mean that the saga handler might not know it needs to initialize
+                    // the saga data, so its better to stop and make things get fixed.
+                    throw new SagaNotStartedException(context.MessageId,
+                        context.SourceQueue,
+                        messageType,
+                        handlerType,
+                        context.SagaKey);
+                }
+            }
+
+            // find the right method on the handler.
+            Type[] parameterTypes = [typeof(IQueueContext), messageType];
+            var handleMethod = UnreachableException.ThrowIfNull(handler.GetType().GetMethod("Handle", parameterTypes),
+                message: "IHandleQueueMessage<> must have a Handle method.");
+
+            // Invoke and await the method.
+            // should it have a seperate try-catch around this and treat it differently?
+            // that would allow us to tell the difference between a problem in a handler, or if the problem was in the bus code.
+            // does that mater for the retry?
+            _log.QueueWork_InvokeHandler(context.MessageId, context.MessageClass, InternalContext.CurrentHandler);
+            {
+                var taskObject = handleMethod.Invoke(handler, [context, context.Message]);
+                var castTask = UnreachableException.ThrowIfNull(taskObject as Task,
+                    message: "IHandleQueueMessage<>.Handle must return a not null Task.");
+                await castTask;
+            }
+
+            if (handlerIsSaga)
+            {
+                await _queueReader.SaveSaga(handler, InternalContext);
+                _log.QueueWork_SagaSaved(InternalContext.CurrentHandler, context.SagaKey!);
             }
         }
     }
