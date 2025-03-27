d2291fb5ababe1b0d192bb37531aea8798155e6c
wleader <william.leader@gmail.com>
2025-03-27 13:30:17 -0400

    Add Fake Clock

---
+++ PeachtreeBus.Tests/Fakes/FakeClock.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PeachtreeBus.Tests.Fakes;
+
+public class FakeClock : ISystemClock
+{
+    public static readonly FakeClock Instance = new();
+
+    public DateTime UtcNow => GetNow();
+
+    public Func<DateTime> GetNow = () => TestData.Now;
+}
