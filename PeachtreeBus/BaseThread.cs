cc96da6e6e82ccce570f0e5aa3347b27e10aa9a9
wleader <william.leader@gmail.com>
2025-03-28 19:32:41 -0400

    File Scoped Namespace

--- PeachtreeBus/BaseThread.cs
+++ PeachtreeBus/BaseThread.cs
@@ -3,85 +3,84 @@ using PeachtreeBus.Data;
 using System;
 using System.Threading.Tasks;
 
-namespace PeachtreeBus
+namespace PeachtreeBus;
+
+/// <summary>
+/// Describes a piece of code that calls an IUnitOFWork in a loop until
+/// signaled to stop.
+/// </summary>
+public interface IThread
 {
-    /// <summary>
-    /// Describes a piece of code that calls an IUnitOFWork in a loop until
-    /// signaled to stop.
-    /// </summary>
-    public interface IThread
+    Task Run();
+}
+
+/// <summary>
+/// Describes something that is called repeatedly from the loop of
+/// and IThread
+/// </summary>
+public interface IUnitOfWork
+{
+    Task<bool> DoWork();
+}
+
+/// <summary>
+/// A basic thread that wrappes the Unit of Work in a database
+/// Transaction.
+/// </summary>
+public abstract class BaseThread(
+    string name,
+    int delayMs,
+    ILogger log,
+    IBusDataAccess dataAccess,
+    IProvideShutdownSignal shutdown)
+    : IThread
+{
+    private readonly string _name = name;
+    private readonly int delayMs = delayMs;
+    private readonly ILogger _log = log;
+    private readonly IBusDataAccess _dataAccess = dataAccess;
+    private readonly IProvideShutdownSignal _shutdown = shutdown;
+
+    public abstract Task<bool> DoUnitOfWork();
+
+    public async Task Run()
     {
-        Task Run();
-    }
-
-    /// <summary>
-    /// Describes something that is called repeatedly from the loop of
-    /// and IThread
-    /// </summary>
-    public interface IUnitOfWork
-    {
-        Task<bool> DoWork();
-    }
-
-    /// <summary>
-    /// A basic thread that wrappes the Unit of Work in a database
-    /// Transaction.
-    /// </summary>
-    public abstract class BaseThread(
-        string name,
-        int delayMs,
-        ILogger log,
-        IBusDataAccess dataAccess,
-        IProvideShutdownSignal shutdown)
-        : IThread
-    {
-        private readonly string _name = name;
-        private readonly int delayMs = delayMs;
-        private readonly ILogger _log = log;
-        private readonly IBusDataAccess _dataAccess = dataAccess;
-        private readonly IProvideShutdownSignal _shutdown = shutdown;
-
-        public abstract Task<bool> DoUnitOfWork();
-
-        public async Task Run()
+        _log.BaseThread_ThreadStart(_name);
+        do
         {
-            _log.BaseThread_ThreadStart(_name);
-            do
+            try
             {
-                try
+                // while reconnecting sounds expensive, its actually not.
+                // the SqlClient library code does connection pooling for us.
+                _dataAccess.Reconnect();
+                _dataAccess.BeginTransaction();
+                if (await DoUnitOfWork())
                 {
-                    // while reconnecting sounds expensive, its actually not.
-                    // the SqlClient library code does connection pooling for us.
-                    _dataAccess.Reconnect();
-                    _dataAccess.BeginTransaction();
-                    if (await DoUnitOfWork())
-                    {
-                        _dataAccess.CommitTransaction();
-                    }
-                    else
-                    {
-                        // there was no work to do.
-                        // Rollback and go to sleep.
-                        _dataAccess.RollbackTransaction();
-                        await Task.Delay(delayMs);
-                    }
+                    _dataAccess.CommitTransaction();
                 }
-                catch (Exception e)
+                else
                 {
-                    _log.BaseThread_ThreadError(_name, e);
-                    try
-                    {
-                        _dataAccess.RollbackTransaction();
-                    }
-                    catch (Exception rollbackEx)
-                    {
-                        _log.BaseThread_RollbackFailed(_name, rollbackEx);
-                    }
+                    // there was no work to do.
+                    // Rollback and go to sleep.
+                    _dataAccess.RollbackTransaction();
+                    await Task.Delay(delayMs);
                 }
             }
-            while (!_shutdown.ShouldShutdown);
-
-            _log.BaseThread_ThreadStop(_name);
+            catch (Exception e)
+            {
+                _log.BaseThread_ThreadError(_name, e);
+                try
+                {
+                    _dataAccess.RollbackTransaction();
+                }
+                catch (Exception rollbackEx)
+                {
+                    _log.BaseThread_RollbackFailed(_name, rollbackEx);
+                }
+            }
         }
+        while (!_shutdown.ShouldShutdown);
+
+        _log.BaseThread_ThreadStop(_name);
     }
 }
