5acb1073ef3dfce79c11c0d8143f762c0d73e39e
wleader <william.leader@gmail.com>
2025-03-31 22:57:27 -0400

    Cleanup

--- PeachtreeBus.Example/Services/ProcessExitShutdownSignal.cs
+++ PeachtreeBus.Example/Services/ProcessExitShutdownSignal.cs
@@ -1,30 +1,29 @@
 ﻿using System;
 using System.Threading;
 
-namespace PeachtreeBus.Example.Services
+namespace PeachtreeBus.Example.Services;
+
+/// <summary>
+/// An Implementation of IProvideShutdownSignal that uses
+/// The Current AppDomain's ProcessExit event.
+/// </summary>
+public class ProcessExitShutdownSignal : IProvideShutdownSignal
 {
+    private readonly CancellationTokenSource _cts = new();
 
-    /// <summary>
-    /// An Implementation of IProvideShutdownSignal that uses
-    /// The Current AppDomain's ProcessExit event.
-    /// </summary>
-    public class ProcessExitShutdownSignal : IProvideShutdownSignal
+    public CancellationToken GetCancellationToken() => _cts.Token;
+
+    public void SignalShutdown() => _cts.Cancel();
+
+    public ProcessExitShutdownSignal()
     {
-        private CancellationTokenSource _cts = new();
-        public CancellationToken GetCancellationToken() => _cts.Token;
-
-        public void SignalShutdown() => _cts.Cancel();
-
-        public ProcessExitShutdownSignal()
-        {
-            AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
-        }
-
-        ~ProcessExitShutdownSignal()
-        {
-            AppDomain.CurrentDomain.ProcessExit -= CurrentDomain_ProcessExit;
-        }
-
-        private void CurrentDomain_ProcessExit(object? sender, EventArgs e) => _cts.Cancel();
+        AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
     }
+
+    ~ProcessExitShutdownSignal()
+    {
+        AppDomain.CurrentDomain.ProcessExit -= CurrentDomain_ProcessExit;
+    }
+
+    private void CurrentDomain_ProcessExit(object? sender, EventArgs e) => _cts.Cancel();
 }
