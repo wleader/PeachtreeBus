615b9f02f4589f3b4c044b12f34e0c3ef61e72cc
wleader <william.leader@gmail.com>
2025-05-02 12:51:42 -0400

    Make visual studio stop warning about namespaces not matching folder structure.

---
+++ PeachtreeBus.MessageInterfaces/Queues/IQueueMessage.cs
@@ -0,0 +1,6 @@
+﻿namespace PeachtreeBus.Queues;
+
+/// <summary>
+/// All queue messages passed through the queue system must implement this interface.
+/// </summary>
+public interface IQueueMessage;
---
+++ PeachtreeBus.MessageInterfaces/Subscriptions/ISubscribedMessage.cs
@@ -0,0 +1,6 @@
+﻿namespace PeachtreeBus.Subscriptions;
+
+/// <summary>
+/// All subscribed messages passed through the subscription system must implement this interface.
+/// </summary>
+public interface ISubscribedMessage { }
