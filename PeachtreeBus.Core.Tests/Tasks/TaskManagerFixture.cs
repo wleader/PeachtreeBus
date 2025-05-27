0c022c350dd0a3d065b2f76db075d1bdbba72ec8
william.leader@gmail.com <william.leader@gmail.com>
2025-05-26 22:18:49 -0400

    Reworked the Task Manager to maintain a list of incomplete tasks. There is a Delay task that is always re-added to the list so the Task.WhenAny will always return at least once a second. This eliminates the need for the sleeper, and removes un-awaited tasks. This was done because there was some kind of glitch with the tasks that when running the the Microsof ApplicationHost, that it didn't respond to Control-C to shut down the process. I'm not sure if that was because of the unawaited tasks, or the lack of a ConfigureAwait(false) somewhere, but this is defintiely less complex code, so thats a win.

--- PeachtreeBus.Core.Tests/Tasks/TaskManagerFixture.cs
+++ PeachtreeBus.Core.Tests/Tasks/TaskManagerFixture.cs
@@ -8,91 +8,49 @@ using System.Threading.Tasks;
 
 namespace PeachtreeBus.Core.Tests.Tasks;
 
-
 [TestClass]
 public class TaskManagerFixture
 {
     private TaskManager _manager = default!;
-    private readonly Mock<ITaskCounter> _counter = new();
     private readonly Mock<IStarters> _starters = new();
-    private readonly Mock<ISleeper> _sleeper = new();
     private CancellationTokenSource _cts = default!;
     private readonly List<Action<Task>> _continues = [];
-    private readonly Queue<int> _availableResults = new();
+    private int _runCount = 0;
 
     [TestInitialize]
     public void Initialize()
     {
-        _counter.Reset();
         _starters.Reset();
 
         _cts = new();
 
-        _counter.Setup(c => c.Available())
-            .Returns(() =>
-            {
-                if (_availableResults.Count == 1)
-                    _cts.Cancel();
-                return _availableResults.TryDequeue(out var result)
-                    ? result
-                    : 0;
-            });
-
-        _sleeper.Setup(s => s.Sleep(It.IsAny<int>()))
-            .Returns(Task.CompletedTask);
-
         _starters.Setup(s => s.RunStarters(It.IsAny<Action<Task>>(), _cts.Token))
             .Callback((Action<Task> continueWith, CancellationToken token) =>
             {
+                _runCount--;
+                if (_runCount == 0) _cts.Cancel();
                 continueWith(Task.CompletedTask);
+            })
+            .ReturnsAsync((Action<Task> continuewith, CancellationToken token) =>
+            {
+                return [Task.Delay(1, CancellationToken.None)
+                    .ContinueWith(continuewith, CancellationToken.None)];
             });
 
         _continues.Clear();
 
         _manager = new(
-            _counter.Object,
-            _starters.Object,
-            _sleeper.Object);
+            _starters.Object);
     }
 
     [TestMethod]
-    public async Task Given_ShouldRunOnce_When_Run_Then_RunsOnce()
+    [DataRow(1)]
+    [DataRow(2)]
+    public async Task Given_ShouldRunNTimes_When_Run_Then_RunsNTimes(int count)
     {
-        _availableResults.Clear();
-        _availableResults.Enqueue(
-            [0, // first wait for available, so causes a sleep
-            1, // second wait for available, breaks the wait loop.
-            1, // causes the second sleep.
-            // token is canceled, so while loop breaks.
-            ]);
-
+        _runCount = count;
         var t = Task.Run(() => _manager.Run(_cts.Token));
-        //_cts.Cancel();
         await t;
-        _starters.Verify(s => s.RunStarters(It.IsAny<Action<Task>>(), _cts.Token), Times.Once);
-        _sleeper.Verify(s => s.Sleep(It.IsAny<int>()), Times.Exactly(2));
-        _sleeper.Verify(s => s.Wake(), Times.Exactly(1));
-    }
-
-
-    [TestMethod]
-    public async Task Given_ShouldRunTwice_When_Run_Then_RunsTwice()
-    {
-        _availableResults.Clear();
-        _availableResults.Enqueue(
-            [0, // first wait for available, so causes a sleep
-            1, // second wait for available, breaks the wait loop.
-            1, // causes the second sleep.
-            1, // wait for available, does not sleep.
-            0, // nothing availabl, skipp second sleep.
-            // token is canceled, so while loop breaks.
-            ]);
-
-        var t = Task.Run(() => _manager.Run(_cts.Token));
-        //_cts.Cancel();
-        await t;
-        _starters.Verify(s => s.RunStarters(It.IsAny<Action<Task>>(), _cts.Token), Times.Exactly(2));
-        _sleeper.Verify(s => s.Sleep(It.IsAny<int>()), Times.Exactly(2));
-        _sleeper.Verify(s => s.Wake(), Times.Exactly(2));
+        _starters.Verify(s => s.RunStarters(It.IsAny<Action<Task>>(), _cts.Token), Times.Exactly(count));
     }
 }
