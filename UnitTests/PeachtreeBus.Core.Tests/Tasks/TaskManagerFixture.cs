9fe9cefc2c721a3db2cb559d3d4043259706d177
william.leader@gmail.com <william.leader@gmail.com>
2025-07-15 09:42:46 -0400

    Forgot to include this file with the previous commit.

--- UnitTests/PeachtreeBus.Core.Tests/Tasks/TaskManagerFixture.cs
+++ UnitTests/PeachtreeBus.Core.Tests/Tasks/TaskManagerFixture.cs
@@ -2,7 +2,6 @@
 using Moq;
 using PeachtreeBus.Tasks;
 using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -16,8 +15,8 @@ public class TaskManagerFixture
     private readonly Mock<IStarters> _starters = new();
     private readonly Mock<IDelayFactory> _delayFactory = new();
     private CancellationTokenSource _cts = default!;
-    private readonly List<Action<Task>> _continues = [];
     private int _runCount = 0;
+    private readonly Task _delayTask = Task.Delay(-1);
 
     [TestInitialize]
     public void Initialize()
@@ -32,14 +31,14 @@ public class TaskManagerFixture
             .Callback((CancellationToken token) =>
             {
                 _runCount--;
-                if (_runCount == 0) _cts.Cancel();
-            })
-            .ReturnsAsync(1);
+                if (_runCount <= 0) _cts.Cancel();
+            });
 
         _delayFactory.Setup(x => x.Delay(It.IsAny<TimeSpan>(), It.IsAny<CancellationToken>()))
-            .Returns(Task.CompletedTask);
+            .Returns(() => _delayTask);
 
-        _continues.Clear();
+        _tasks.Setup(t => t.WhenAll()).Returns(Task.CompletedTask);
+        _tasks.Setup(t => t.WhenAny()).Returns(Task.CompletedTask);
 
         _manager = new(
             _tasks.Object,
@@ -47,39 +46,29 @@ public class TaskManagerFixture
             _starters.Object);
     }
 
-    private void StartersCallback(CancellationToken token)
-    {
-        _runCount--;
-        if (_runCount <= 0) _cts.Cancel();
-    }
-
     [TestMethod]
-    [DataRow(1)]
-    [DataRow(2)]
-    public async Task Given_ShouldRunNTimes_When_Run_Then_RunsNTimes(int count)
-    {
-        _runCount = count;
-        var t = Task.Run(() => _manager.Run(_cts.Token));
-        await t;
-        _starters.Verify(s => s.RunStarters(_cts.Token), Times.Exactly(count));
-    }
-
-    [TestMethod]
-    [DataRow(1)]
-    [DataRow(2)]
-    public async Task Given_StartersReturnsZero_When_Run_Then_Delay(int count)
+    [DataRow(1, 0, 1, DisplayName = "RunOnceExpectDelay")]
+    [DataRow(1, 1, 0, DisplayName = "RunOnceNoDelay")]
+    [DataRow(2, 2, 0, DisplayName = "RunTwiceNoDelay")]
+    [DataRow(2, 0, 2, DisplayName = "RunTwiceExpectDelay")]
+    public async Task Given_RunCount_And_TaskCount_When_Run_Then_StartCount_And_DelayCount(int count, int taskCount, int expectedDelays)
     {
         _runCount = count;
 
-        _starters.Setup(s => s.RunStarters(_cts.Token))
-            .Callback(StartersCallback)
-            .ReturnsAsync(0);
+        _tasks.SetupGet(t => t.Count).Returns(taskCount);
 
         var t = Task.Run(() => _manager.Run(_cts.Token));
         await t;
 
         _starters.Verify(s => s.RunStarters(_cts.Token), Times.Exactly(count));
-        _delayFactory.Verify(x => x.Delay(TimeSpan.FromSeconds(1), CancellationToken.None), Times.Exactly(count));
-        _tasks.Verify(t => t.Add(Task.CompletedTask), Times.Exactly(count));
+        _tasks.Verify(t => t.WhenAny(), Times.Exactly(count));
+        _tasks.Verify(t => t.WhenAll(), Times.Once);
+
+        _delayFactory.Verify(x => x.Delay(
+            TimeSpan.FromSeconds(1),
+            CancellationToken.None),
+            Times.Exactly(expectedDelays));
+
+        _tasks.Verify(t => t.Add(_delayTask), Times.Exactly(expectedDelays));
     }
 }
