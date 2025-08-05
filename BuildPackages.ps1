df1d3ffc56d9d583f91f80f6875a7ba15ba46b8f
william.leader@gmail.com <william.leader@gmail.com>
2025-08-04 22:52:35 -0400

    Updated the Build Packages Script to set the version and release notes in the nuspec files. Also Sets the version in Directory.Build.props. Also updates the nuspect dependency versions from the Directory.Packages.props.

--- BuildPackages.ps1
+++ BuildPackages.ps1
@@ -1,3 +1,13 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$releaseNotes,
+
+    [Parameter(Mandatory=$true)]
+    [string]$version
+)
+
+$ErrorActionPreference = "Stop"
+
 function Quiet-Remove-Folder
 {
     param ([string]$folder)
@@ -9,6 +19,63 @@ function Quiet-Remove-Folder
     {
         Write-Error "Folder not removed: $folder"
     }
+}
+
+function Get-DirectoryPackagesVersion
+{
+    param([string]$id)
+    $fullPath = Resolve-Path ".\Directory.Packages.props"
+    $xml = New-Object -TypeName System.Xml.XmlDocument
+    $xml.PreserveWhitespace = $true
+    $xml.Load($fullPath)
+    $node = $xml.SelectNodes("//PackageVersion[@Include='$id']")
+    $result = $node.GetAttribute("Version")
+    return $result
+}
+
+function Update-Nuspec
+{
+    param([string]$version,[string]$filename)
+    $fullPath = Resolve-Path $filename
+
+    $xml = New-Object -TypeName System.Xml.XmlDocument
+    $xml.PreserveWhitespace = $true
+    $xml.Load($fullPath)
+    [System.Xml.XmlNamespaceManager]$ns = $xml.NameTable
+    $ns.AddNamespace("nuspec", $xml.DocumentElement.NamespaceURI)
+
+    $xml.package.metadata.version = $version
+    $xml.package.metadata.releaseNotes = ($version + ' ' + $releaseNotes)
+
+    $dependencyNodes = $xml.SelectNodes("//nuspec:dependency",$ns)
+    foreach($node in $dependencyNodes)
+    {
+        $id = $node.GetAttribute("id")
+        if($id.StartsWith("PeachtreeBus.")){
+            $node.SetAttribute('version', $version)
+        }
+        else
+        {
+            $packageVersion = Get-DirectoryPackagesVersion $id
+            $node.SetAttribute('version', $packageVersion)
+        }
+    }
+
+    $xml.Save($fullPath)
+}
+
+function Update-DirectoryBuildProps
+{
+    $fullPath = Resolve-Path ".\Directory.Build.props"
+    $xml = New-Object -TypeName System.Xml.XmlDocument
+    $xml.PreserveWhitespace = $true
+    $xml.Load($fullPath)
+
+    $xml.Project.PropertyGroup.Version = $version
+    $xml.Project.PropertyGroup.FileVersion = $version
+    $xml.Project.PropertyGroup.AssemblyVersion = ($version +".0")
+
+    $xml.Save($fullPath)
 }
 
 Quiet-Remove-Folder .\PeachtreeBus.MessageInterfaces\bin\Debug
@@ -23,6 +90,15 @@ Quiet-Remove-Folder .\PeachtreeBus.MicrosoftDependencyInjection\bin\Debug
 Quiet-Remove-Folder .\PeachtreeBus.MicrosoftDependencyInjection\bin\Release
 Quiet-Remove-Folder .\PeachtreeBus.EntityFrameworkCore\bin\Debug
 Quiet-Remove-Folder .\PeachtreeBus.EntityFrameworkCore\bin\Release
+
+Update-DirectoryBuildProps
+
+Update-Nuspec $version ".\PeachtreeBus.MessageInterfaces.nuspec"
+Update-Nuspec $version ".\PeachtreeBus.Abstractions.nuspec"
+Update-Nuspec $version ".\PeachtreeBus.Core.nuspec"
+Update-Nuspec $version ".\PeachtreeBus.SimpleInjector.nuspec"
+Update-Nuspec $version ".\PeachtreeBus.MicrosoftDependencyInjection.nuspec"
+Update-Nuspec $version ".\PeachtreeBus.EntityFrameworkCore.nuspec"
 
 .\nuget.exe restore PeachtreeBus.sln
 
