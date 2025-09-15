111ee5c48582a515157458d6535321cfc2d430d6
wleader <william.leader@gmail.com>
2025-09-15 12:05:04 -0400

    Create SemaphoreSlim extensions. Basically A Try Finally around an Action or Func<>

---
+++ PeachtreeBus.Core/Tasks/SemaphoreSlimExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PeachtreeBus.Tasks;
+
+public static class SemaphoreSlimExtensions
+{
+    public static void Invoke(this SemaphoreSlim semaphore, Action? action)
+    {
+        semaphore.Wait();
+        try
+        {
+            action?.Invoke();
+        }
+        finally
+        {
+            semaphore.Release();
+        }
+    }
+
+    public static T Invoke<T>(this SemaphoreSlim semaphore, Func<T> func)
+    {
+        semaphore.Wait();
+        try
+        {
+            return func.Invoke();
+        }
+        finally
+        {
+            semaphore.Release();
+        }
+    }
+}
